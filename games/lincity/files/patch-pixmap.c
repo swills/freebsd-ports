swills/freebsd-ports

commit 3c17558c098e488b6f14422eb30ad37eecce48e9

Fix build with clang
git-svn-id: svn+ssh://svn.freebsd.org/ports/head@324073 35697150-7ecd-e111-bb59-0022644237b5

games/lincity/Makefile
@@ -14,11 +14,8 @@ OPTIONS_DEFINE=	SVGALIB NLS
 
 LIB_DEPENDS=	png15:${PORTSDIR}/graphics/png
 
-.if ${CC} == "clang"
+USES=		gmake
 USE_CSTD=	c89
-.endif
-
-USE_GMAKE=	yes
 GNU_CONFIGURE=	yes
 CPPFLAGS+=	-I${LOCALBASE}/include
 LDFLAGS+=	-L${LOCALBASE}/lib

commit 0ba3658c37956d93ad111e1e6500380e0ea8fc30

- Fix build breakage introduced in r324073 - (Really) fix build with clang

games/lincity/Makefile
@@ -10,25 +10,21 @@ MASTER_SITES=	SF/${PORTNAME}/Lincity%20Development%20Source/${PORTVERSION}
 MAINTAINER=	ports@FreeBSD.org
 COMMENT=	Rich city simulation game for X
 
-OPTIONS_DEFINE=	SVGALIB NLS
-
 LIB_DEPENDS=	png15:${PORTSDIR}/graphics/png
 
 USES=		gmake
-USE_CSTD=	c89
 GNU_CONFIGURE=	yes
 CPPFLAGS+=	-I${LOCALBASE}/include
 LDFLAGS+=	-L${LOCALBASE}/lib
 
 MAN6=		lincity.6
 
+OPTIONS_DEFINE=	SVGALIB NLS
+
 .include <bsd.port.options.mk>
 
-pre-configure:
-	@${REINPLACE_CMD} -e 's|malloc.h|stdlib.h|g' ${WRKSRC}/readpng.c
-
 .if ${PORT_OPTIONS:MSVGALIB}
-LIB_DEPENDS+=	vga.1:${PORTSDIR}/graphics/svgalib
+LIB_DEPENDS+=	vga:${PORTSDIR}/graphics/svgalib
 CONFIGURE_ARGS+=	--without-x
 PLIST_SUB=	PROG="bin/lincity"
 .else
@@ -44,5 +40,8 @@ PLIST_SUB+=	NLS=""
 CONFIGURE_ARGS+=--disable-nls
 PLIST_SUB+=	NLS="@comment "
 .endif
+
+pre-configure:
+	@${REINPLACE_CMD} -e 's|malloc.h|stdlib.h|g' ${WRKSRC}/readpng.c
 
 .include <bsd.port.mk>

games/lincity/files/patch-pixmap.c
@@ -1,0 +1,11 @@
+--- pixmap.c.orig	2003-03-17 01:32:40.000000000 +0300
++++ pixmap.c	2013-08-28 22:15:03.677227606 +0400
+@@ -390,7 +390,7 @@
+ 		  likes it in the file.  Still need to fix this in autoconf,
+                   but for now this will do. */
+ #if !defined (WIN32)
+-inline int 
++int 
+ pixmap_index (int x, int y)
+ {
+     return y*pixmap_width + x;

games/lincity/files/patch-pixmap.h
@@ -1,0 +1,11 @@
+--- pixmap.h.orig	2003-02-03 06:52:18.000000000 +0300
++++ pixmap.h	2013-08-28 22:14:20.285227378 +0400
+@@ -19,7 +19,7 @@
+ void initialize_pixmap (void);
+ void resize_pixmap (int new_width, int new_height);
+ int pixmap_getpixel (int x, int y);
+-inline int pixmap_index (int x, int y);
++int pixmap_index (int x, int y);
+ int pixmap_getpixel (int x, int y);
+ void pixmap_setpixel (int x, int y, int col);
+ void pixmap_hline (int x1, int y1, int x2, int col);