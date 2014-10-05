swills/freebsd-ports

commit 88afe48652459eb6487af6cf52a3f04e6f4e943c

Replace USE_PYTHON* by USES=python.
This commit consists of area51's r10314 and r10356 patchsets.

Approved by:	rakuco (mentor)


git-svn-id: svn+ssh://svn.freebsd.org/ports/head@370100 35697150-7ecd-e111-bb59-0022644237b5

devel/py-qt4/bsd.pyqt.mk
@@ -93,7 +93,7 @@ DISTNAME=	${PYQT4_DISTNAME}
 
 DISTINFO_FILE=	${PYQT4_DISTINFO_FILE}
 HAS_CONFIGURE=	yes
-USE_PYTHON=	yes
+USES=		python
 
 QT_NONSTANDARD=	yes  # Do not add unknown arguments to CONFIGURE_ARGS.
 

devel/py-sip/Makefile
@@ -12,8 +12,7 @@ DISTNAME=	${SIP_DISTNAME}
 MAINTAINER=	kde@FreeBSD.org
 COMMENT=	Python to C and C++ bindings generator
 
-USE_PYTHON=		yes
-PYTHON_PY3K_PLIST_HACK=	yes
+USE=		python:py3kplist
 
 DATADIR=	${PREFIX}/share/py-${PORTNAME}
 DOCSDIR=	${PREFIX}/share/doc/py-${PORTNAME}

deskutils/superkaramba/Makefile
@@ -10,9 +10,8 @@ DIST_SUBDIR=	KDE/${PORTVERSION}
 MAINTAINER=	kde@FreeBSD.org
 COMMENT=	Widget framework for KDE
 
-USE_PYTHON=	2
 USE_KDE4=	kdehier kdelibs kdeprefix automoc4 qimageblitz
-USES=		cmake:outsource tar:xz
+USES=		cmake:outsource python:2 tar:xz
 USE_QT4=	qmake_build moc_build rcc_build uic_build
 USE_LDCONFIG=	yes
 

devel/py-krosspython/Makefile
@@ -13,9 +13,8 @@ MAINTAINER=	kde@FreeBSD.org
 COMMENT=	Kross Python scripting library
 
 USE_KDE4=	kdehier kdelibs kdeprefix automoc4
-USE_PYTHON=	2
 USE_QT4=	corelib gui qmake_build moc_build uic_build rcc_build
-USES=		cmake:outsource tar:xz
+USES=		cmake:outsource python:2 tar:xz
 CMAKE_ARGS+=	-DBUILD_falcon:BOOL=FALSE \
 		-DBUILD_ruby:BOOL=FALSE \
 		-DBUILD_java:BOOL=FALSE

devel/py-pykde4/Makefile
@@ -28,10 +28,9 @@ RUN_DEPENDS:=	${BUILD_DEPENDS} \
 
 USE_KDE4=	kdehier kdelibs kdeprefix okular pimlibs \
 		akonadi automoc4 soprano
-USE_PYTHON=	2
 USE_QT4=	corelib dbus designer_build gui network xml \
 		qmake_build moc_build uic_build rcc_build
-USES=		cmake:outsource tar:xz
+USES=		cmake:outsource python:2 tar:xz
 CMAKE_ARGS+=	-DWITH_PolkitQt:BOOL=FALSE
 
 SUB_FILES=	kdebindings4.pth

devel/py-pykdeuic4/Makefile
@@ -13,10 +13,9 @@ MAINTAINER=	kde@FreeBSD.org
 COMMENT=	Enhanced version of pyuic4
 
 USE_KDE4=	kdelibs automoc4 pykde4
-USE_PYTHON=	2
 USE_QT4=	moc_build qmake_build rcc_build uic_build
 DISTINFO_FILE=	${PORTSDIR}/devel/py-pykde4/distinfo
-USES=		cmake:outsource tar:xz
+USES=		cmake:outsource python:2 tar:xz
 
 WRKSRC=		${WRKDIR}/${DISTNAME}/tools/${PORTNAME}
 

devel/py-qt4-qscintilla2/Makefile
@@ -22,7 +22,7 @@ RUN_DEPENDS:=	${BUILD_DEPENDS}
 DESTDIRNAME=	INSTALL_ROOT
 DISTINFO_FILE=	${QSCI2_DISTINFO_FILE}
 HAS_CONFIGURE=	yes
-USE_PYTHON=	yes
+USES=		python
 USE_QT4=	gui moc_build qmake_build
 QT_NONSTANDARD=	yes  # Do not add unknown arguments to CONFIGURE_ARGS.
 

devel/py-qt4/Makefile
@@ -13,7 +13,7 @@ EXTRACT_ONLY=	# empty
 MAINTAINER=	kde@FreeBSD.org
 COMMENT=	Python bindings for the Qt 4 toolkit (meta port)
 
-USE_PYTHON=	yes
+USES=		python
 
 NO_BUILD=	yes
 NO_INSTALL=	yes

commit 1ff2d98007ef32d17cb06e46377b63c92586436f

Fix USES issues.
(Hopefuly) fix issues introduced in r370100. This commit consists of area51
revisions 10357, 10359 and 10360.

Approved by:	rakuco (mentor)

devel/py-qt4/bsd.pyqt.mk
@@ -93,7 +93,7 @@ DISTNAME=	${PYQT4_DISTNAME}
 
 DISTINFO_FILE=	${PYQT4_DISTINFO_FILE}
 HAS_CONFIGURE=	yes
-USES=		python
+USES+=		python
 
 QT_NONSTANDARD=	yes  # Do not add unknown arguments to CONFIGURE_ARGS.
 

devel/py-sip/Makefile
@@ -12,7 +12,8 @@ DISTNAME=	${SIP_DISTNAME}
 MAINTAINER=	kde@FreeBSD.org
 COMMENT=	Python to C and C++ bindings generator
 
-USE=		python:py3kplist
+USES=		python
+USE_PYTHON=	py3kplist
 
 DATADIR=	${PREFIX}/share/py-${PORTNAME}
 DOCSDIR=	${PREFIX}/share/doc/py-${PORTNAME}