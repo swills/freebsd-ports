swills/freebsd-ports

commit de3106015c2f56a3ab8410d34e815450938d1891

Upgrade to release 2.2.0
PR:		ports/192046
Submitted by:	rodrigo


git-svn-id: svn+ssh://svn.freebsd.org/ports/head@374261 35697150-7ecd-e111-bb59-0022644237b5

graphics/nomacs/Makefile
@@ -1,8 +1,7 @@
 # $FreeBSD$
 
 PORTNAME=	nomacs
-PORTVERSION=	1.6.4
-PORTREVISION=	1
+PORTVERSION=	2.2.0
 CATEGORIES=	graphics
 MASTER_SITES=	SF/nomacs/nomacs-${PORTVERSION}/
 DISTNAME=	${PORTNAME}-${PORTVERSION}-source

graphics/nomacs/files/patch-cmake_Unix.cmake
@@ -1,0 +1,54 @@
+--- cmake/Unix.cmake.orig	2014-11-11 12:00:55.000000000 +0000
++++ cmake/Unix.cmake	2014-12-05 08:24:29.222613898 +0000
+@@ -34,17 +34,15 @@
+ endif(NOT EXIV2_FOUND)
+ 
+ # search for opencv
+-unset(OpenCV_FOUND CACHE)
++unset(OPENCVCORE_FOUND CACHE)
+ if(ENABLE_OPENCV)
+-	find_package(OpenCV 2.1.0 REQUIRED core imgproc)
+-	if (NOT OpenCV_LIBRARIES) # OpenCV_FOUND can not be used since it is set in Ubuntu 12.04 (without finding opencv)
+-		# Older OpenCV versions only supplied pkg-config files
+-		if(PKG_CONFIG_FOUND)
+-			pkg_check_modules(OpenCV opencv>=2.1.0)
+-		endif()
++	find_package(OpenCVCore COMPONENTS core imgproc REQUIRED)
++
++	if (NOT OPENCV_CORE_LIBRARY)
++		message(FATAL_ERROR "OpenCV libraries not found, but requested.")
+ 	endif()
+ 
+-	if(NOT OpenCV_FOUND)
++	if (NOT OPENCVCORE_FOUND)
+ 		message(FATAL_ERROR "OpenCV not found, but requested.")
+ 	endif()
+ 
+@@ -56,7 +54,7 @@
+ 
+ # search for libraw
+ if(ENABLE_RAW)
+-	if(NOT OpenCV_FOUND)
++	if(NOT OPENCVCORE_FOUND)
+ 		message(FATAL_ERROR "OpenCV is mandotory when enabling RAW. You have to enable ENABLE_OPENCV")
+ 	endif()
+ 
+@@ -72,7 +70,7 @@
+ unset(TIFF_INCLUDE_DIR CACHE)
+ unset(TIFF_LIBRARY CACHE)
+ if(ENABLE_TIFF)
+-	if(NOT OpenCV_FOUND)
++	if(NOT OPENCVCORE_FOUND)
+ 		message(FATAL_ERROR "OpenCV is mandotory when enabling TIFF. You have to enable ENABLE_OPENCV")
+ 	endif()
+ 	find_package(TIFF)
+@@ -84,6 +82,9 @@
+ 	endif()
+ endif(ENABLE_TIFF)
+ 
++# sysinfo library
++FIND_LIBRARY(SYSINFO_LIBRARY NAMES sysinfo)
++
+ #search for quazip
+ unset(QUAZIP_SOURCE_DIRECTORY CACHE)
+ unset(QUAZIP_INCLUDE_DIRECTORY CACHE)

graphics/nomacs/files/patch-cmake_UnixBuildTarget.cmake
@@ -1,0 +1,29 @@
+--- cmake/UnixBuildTarget.cmake.orig	2014-12-04 21:12:50.516406428 +0000
++++ cmake/UnixBuildTarget.cmake	2014-12-04 21:14:12.825377661 +0000
+@@ -3,7 +3,7 @@
+   set(BINARY_NAME ${CMAKE_PROJECT_NAME})
+   link_directories(${LIBRAW_LIBRARY_DIRS} ${OpenCV_LIBRARY_DIRS} ${EXIV2_LIBRARY_DIRS})
+   add_executable(${BINARY_NAME} WIN32 MACOSX_BUNDLE ${NOMACS_SOURCES} ${NOMACS_UI} ${NOMACS_MOC_SRC} ${NOMACS_RCC} ${NOMACS_HEADERS} ${NOMACS_RC} ${NOMACS_QM} ${NOMACS_TRANSLATIONS} ${LIBQPSD_SOURCES} ${LIBQPSD_HEADERS} ${LIBQPSD_MOC_SRC} ${WEBP_SOURCE} ${QUAZIP_SOURCES} ${QUAZIP_MOC_SRC})
+-  target_link_libraries(${BINARY_NAME} ${QT_LIBRARIES} ${EXIV2_LIBRARIES} ${LIBRAW_LIBRARIES} ${OpenCV_LIBRARIES} ${VERSION_LIB} ${TIFF_LIBRARY} ${ZLIB_LIBRARY})
++  target_link_libraries(${BINARY_NAME} ${QT_LIBRARIES} ${EXIV2_LIBRARIES} ${LIBRAW_LIBRARIES} ${OPENCV_CORE_LIBRARY} ${OPENCV_IMGPROC_LIBRARY} ${SYSINFO_LIBRARY} ${VERSION_LIB} ${TIFF_LIBRARY} ${ZLIB_LIBRARY})
+ 
+   if(CMAKE_SYSTEM_NAME MATCHES "Linux")
+ 	  SET_TARGET_PROPERTIES(${BINARY_NAME} PROPERTIES LINK_FLAGS -fopenmp)
+@@ -49,7 +49,7 @@
+   set(DLL_NAME lib${CMAKE_PROJECT_NAME})
+   #set(LIB_NAME optimized ${DLL_NAME}.lib debug ${DLL_NAME}d.lib)
+   LIST(REMOVE_ITEM NOMACS_SOURCES ${CMAKE_SOURCE_DIR}/src/main.cpp)
+-  link_directories(${LIBRAW_LIBRARY_DIRS} ${OpenCV_LIBRARY_DIRS} ${EXIV2_LIBRARY_DIRS} ${CMAKE_BINARY_DIR})
++  link_directories(${LIBRAW_LIBRARY_DIRS} ${SYSINFO_LIBRARY_DIRS}  ${OPENCV_CORE_LIBRARY_DIRS} ${EXIV2_LIBRARY_DIRS} ${CMAKE_BINARY_DIR})
+   add_executable(${BINARY_NAME} WIN32  MACOSX_BUNDLE src/main.cpp ${NOMACS_MOC_SRC_SU} ${NOMACS_QM} ${NOMACS_TRANSLATIONS} ${NOMACS_RC})
+   target_link_libraries(${BINARY_NAME} ${QT_LIBRARIES} ${VERSION_LIB} ${DLL_NAME})
+ 
+@@ -57,7 +57,7 @@
+   set_target_properties(${BINARY_NAME} PROPERTIES IMPORTED_IMPLIB "")
+ 		  
+   add_library(${DLL_NAME} SHARED ${NOMACS_SOURCES} ${NOMACS_UI} ${NOMACS_MOC_SRC} ${NOMACS_RCC} ${NOMACS_HEADERS} ${NOMACS_RC} ${LIBQPSD_SOURCES} ${LIBQPSD_HEADERS} ${LIBQPSD_MOC_SRC} ${WEBP_SOURCE}  ${QUAZIP_SOURCES} ${QUAZIP_MOC_SRC})
+-  target_link_libraries(${DLL_NAME} ${QT_LIBRARIES} ${EXIV2_LIBRARIES} ${LIBRAW_LIBRARIES} ${OpenCV_LIBRARIES} ${VERSION_LIB} ${TIFF_LIBRARIES} ${HUPNP_LIBS} ${HUPNPAV_LIBS})
++  target_link_libraries(${DLL_NAME} ${QT_LIBRARIES} ${EXIV2_LIBRARIES} ${LIBRAW_LIBRARIES} ${OPENCV_CORE_LIBRARY} ${OPENCV_IMGPROC_LIBRARY} ${SYSINFO_LIBRARY} ${VERSION_LIB} ${TIFF_LIBRARIES} ${HUPNP_LIBS} ${HUPNPAV_LIBS})
+   add_dependencies(${BINARY_NAME} ${DLL_NAME})
+ 
+   if (ENABLE_QT5)

graphics/nomacs/pkg-plist
@@ -1,5 +1,4 @@
 bin/nomacs
-man/man1/nomacs.1.gz
 share/applications/nomacs.desktop
 %%DATADIR%%/translations/nomacs_als.qm
 %%DATADIR%%/translations/nomacs_az.qm

graphics/nomacs/distinfo
@@ -1,2 +1,2 @@
-SHA256 (nomacs-1.6.4-source.tar.bz2) = 85bb13567165af08f699ee87630151bf2a81d77cd75e025a7a332ab9c9022a94
-SIZE (nomacs-1.6.4-source.tar.bz2) = 1173684
+SHA256 (nomacs-2.2.0-source.tar.bz2) = 10825140bd856334aa3ba3a59c93f7205ec9218321d5ded20a70a43af424a884
+SIZE (nomacs-2.2.0-source.tar.bz2) = 1392489

commit 0338d4d42e6baf607e4e5e8fb458f8a06194926f

Fix problems introduced in r374261.
* Fix the plist.
* Add patch to properly look for and link against pthreads, required by the
  bundled webp copy used by the port (this fixes the build on FreeBSD >=
  10). Ideally, we should just use webp from ports instead.

PR:		192046

graphics/nomacs/Makefile
@@ -2,6 +2,7 @@
 
 PORTNAME=	nomacs
 PORTVERSION=	2.2.0
+PORTREVISION=	1
 CATEGORIES=	graphics
 MASTER_SITES=	SF/nomacs/nomacs-${PORTVERSION}/
 DISTNAME=	${PORTNAME}-${PORTVERSION}-source

graphics/nomacs/files/patch-cmake_Unix.cmake
@@ -1,5 +1,5 @@
---- cmake/Unix.cmake.orig	2014-11-11 12:00:55.000000000 +0000
-+++ cmake/Unix.cmake	2014-12-05 08:24:29.222613898 +0000
+--- cmake/Unix.cmake
++++ cmake/Unix.cmake
 @@ -34,17 +34,15 @@
  endif(NOT EXIV2_FOUND)
  
@@ -52,3 +52,12 @@
  #search for quazip
  unset(QUAZIP_SOURCE_DIRECTORY CACHE)
  unset(QUAZIP_INCLUDE_DIRECTORY CACHE)
+@@ -128,6 +129,8 @@
+ SET(WEBP_INCLUDE_DIR "")
+ SET(WEBP_SOURCE "")
+ if(ENABLE_WEBP)
++	find_package(Threads REQUIRED)
++
+ 	add_definitions(-DNDEBUG -DWEBP_USE_THREAD)
+ 
+ 	file(GLOB WEBP_DEC_SRCS

graphics/nomacs/files/patch-cmake_UnixBuildTarget.cmake
@@ -1,11 +1,11 @@
---- cmake/UnixBuildTarget.cmake.orig	2014-12-04 21:12:50.516406428 +0000
-+++ cmake/UnixBuildTarget.cmake	2014-12-04 21:14:12.825377661 +0000
+--- cmake/UnixBuildTarget.cmake
++++ cmake/UnixBuildTarget.cmake
 @@ -3,7 +3,7 @@
    set(BINARY_NAME ${CMAKE_PROJECT_NAME})
    link_directories(${LIBRAW_LIBRARY_DIRS} ${OpenCV_LIBRARY_DIRS} ${EXIV2_LIBRARY_DIRS})
    add_executable(${BINARY_NAME} WIN32 MACOSX_BUNDLE ${NOMACS_SOURCES} ${NOMACS_UI} ${NOMACS_MOC_SRC} ${NOMACS_RCC} ${NOMACS_HEADERS} ${NOMACS_RC} ${NOMACS_QM} ${NOMACS_TRANSLATIONS} ${LIBQPSD_SOURCES} ${LIBQPSD_HEADERS} ${LIBQPSD_MOC_SRC} ${WEBP_SOURCE} ${QUAZIP_SOURCES} ${QUAZIP_MOC_SRC})
 -  target_link_libraries(${BINARY_NAME} ${QT_LIBRARIES} ${EXIV2_LIBRARIES} ${LIBRAW_LIBRARIES} ${OpenCV_LIBRARIES} ${VERSION_LIB} ${TIFF_LIBRARY} ${ZLIB_LIBRARY})
-+  target_link_libraries(${BINARY_NAME} ${QT_LIBRARIES} ${EXIV2_LIBRARIES} ${LIBRAW_LIBRARIES} ${OPENCV_CORE_LIBRARY} ${OPENCV_IMGPROC_LIBRARY} ${SYSINFO_LIBRARY} ${VERSION_LIB} ${TIFF_LIBRARY} ${ZLIB_LIBRARY})
++  target_link_libraries(${BINARY_NAME} ${QT_LIBRARIES} ${EXIV2_LIBRARIES} ${LIBRAW_LIBRARIES} ${OPENCV_CORE_LIBRARY} ${OPENCV_IMGPROC_LIBRARY} ${SYSINFO_LIBRARY} ${VERSION_LIB} ${TIFF_LIBRARY} ${ZLIB_LIBRARY} ${CMAKE_THREAD_LIBS_INIT})
  
    if(CMAKE_SYSTEM_NAME MATCHES "Linux")
  	  SET_TARGET_PROPERTIES(${BINARY_NAME} PROPERTIES LINK_FLAGS -fopenmp)

graphics/nomacs/pkg-plist
@@ -1,5 +1,6 @@
 bin/nomacs
 share/applications/nomacs.desktop
+share/man/man1/nomacs.1
 %%DATADIR%%/translations/nomacs_als.qm
 %%DATADIR%%/translations/nomacs_az.qm
 %%DATADIR%%/translations/nomacs_cs.qm
@@ -8,6 +9,7 @@ share/applications/nomacs.desktop
 %%DATADIR%%/translations/nomacs_es.qm
 %%DATADIR%%/translations/nomacs_fr.qm
 %%DATADIR%%/translations/nomacs_it.qm
+%%DATADIR%%/translations/nomacs_ja.qm
 %%DATADIR%%/translations/nomacs_pl.qm
 %%DATADIR%%/translations/nomacs_pt.qm
 %%DATADIR%%/translations/nomacs_ru.qm