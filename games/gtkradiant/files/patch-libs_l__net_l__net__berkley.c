swills/freebsd-ports

commit b2149080925d8d6c875a638f1e95f84b475c324c

- Fix build on 64 bit architectures (cast from pointer to int loses precision) - Drop @dirrm* from plist - Add empty directories to plist
Approved by:	portmgr blanket


git-svn-id: svn+ssh://svn.freebsd.org/ports/head@384527 35697150-7ecd-e111-bb59-0022644237b5

games/gtkradiant/files/patch-radiant_treemodel.cpp
@@ -9,7 +9,7 @@
 +  // C++ code, and Clang will assume that comparison below always evaluates
 +  // to true, resulting in a segmentation fault.  Use a dirty hack to force
 +  // Clang to check those "bad" references for null nonetheless.
-+  volatile int n = (int)&node;
++  volatile intptr_t n = (intptr_t)&node;
 +
 +  if(n != 0)
    {
@@ -20,7 +20,7 @@
  void node_detach_name_changed_callback(scene::Node& node, const NameCallback& callback)
  {
 -  if(&node != 0)
-+  volatile int n = (int)&node;		// see the comment on line 713
++  volatile intptr_t n = (intptr_t)&node;		// see the comment on line 713
 +
 +  if(n != 0)
    {
@@ -31,7 +31,7 @@
  const char* node_get_name_safe(scene::Node& node)
  {
 -  if(&node == 0)
-+  volatile int n = (int)&node;		// see the comment on line 713
++  volatile intptr_t n = (intptr_t)&node;		// see the comment on line 713
 +
 +  if(n == 0)
    {
@@ -42,7 +42,7 @@
  void node_attach_name_changed_callback(scene::Node& node, const NameCallback& callback)
  {
 -  if(&node != 0)
-+  volatile int n = (int)&node;		// see the comment on line 713
++  volatile intptr_t n = (intptr_t)&node;		// see the comment on line 713
 +
 +  if(n != 0)
    {
@@ -53,7 +53,7 @@
  void node_detach_name_changed_callback(scene::Node& node, const NameCallback& callback)
  {
 -  if(&node != 0)
-+  volatile int n = (int)&node;		// see the comment on line 713
++  volatile intptr_t n = (intptr_t)&node;		// see the comment on line 713
 +
 +  if(n != 0)
    {

games/gtkradiant/pkg-plist
@@ -177,43 +177,6 @@ bin/gtkradiant
 %%DATADIR%%/wolf.game/main/default_shaderlist.txt
 %%DATADIR%%/wolf.game/main/wolf_entities.def
 %%DATADIR%%/wolf.game/main/wolf_entities.ent
-@dirrm %%DATADIR%%/wolf.game/main
-@dirrm %%DATADIR%%/wolf.game
-@dirrm %%DATADIR%%/stvef.game/baseEF
-@dirrm %%DATADIR%%/stvef.game
-@dirrm %%DATADIR%%/sof2.game/base
-@dirrm %%DATADIR%%/sof2.game
-@dirrm %%DATADIR%%/q4.game/q4base
-@dirrm %%DATADIR%%/q4.game
-@dirrm %%DATADIR%%/q3bp.game/baseq3
-@dirrm %%DATADIR%%/q3bp.game
-@dirrm %%DATADIR%%/q3.game/missionpack
-@dirrm %%DATADIR%%/q3.game/baseq3
-@dirrm %%DATADIR%%/q3.game
-@dirrm %%DATADIR%%/q2.game/baseq2
-@dirrm %%DATADIR%%/q2.game
-@dirrm %%DATADIR%%/q1.game/id1
-@dirrm %%DATADIR%%/q1.game
-@dirrm %%DATADIR%%/modules
-@dirrm %%DATADIR%%/jk2.game/base
-@dirrm %%DATADIR%%/jk2.game
-@dirrm %%DATADIR%%/jabp.game
-@dirrm %%DATADIR%%/ja.game/base
-@dirrm %%DATADIR%%/ja.game
-@dirrm %%DATADIR%%/hlcs.game
-@dirrm %%DATADIR%%/hl.game/valve
-@dirrm %%DATADIR%%/hl.game/cstrike
-@dirrm %%DATADIR%%/hl.game
-@dirrm %%DATADIR%%/heretic2.game/base
-@dirrm %%DATADIR%%/heretic2.game
-@dirrm %%DATADIR%%/heretic2
-@dirrm %%DATADIR%%/gl
-@dirrm %%DATADIR%%/games
-@dirrm %%DATADIR%%/et.game/etmain
-@dirrm %%DATADIR%%/et.game/bitmaps
-@dirrm %%DATADIR%%/et.game
-@dirrm %%DATADIR%%/doom3.game/base
-@dirrm %%DATADIR%%/doom3.game
-@dirrm %%DATADIR%%/bitmaps
-@dirrm %%DATADIR%%/plugins
-@dirrm %%DATADIR%%
+@dir %%DATADIR%%/hlcs.game
+@dir %%DATADIR%%/jabp.game
+@dir %%DATADIR%%/plugins

commit 0b8cf27e3c1fed71985bb633a10473ea67272317

- Fix broken communication with external tools (e.g. q2map): GtkRadiant's   network library (libs/l_net) defines its own struct sockaddr_s, which   has single ``short sa_family'' field instead of ``unsigned char sa_len''   and ``sa_family_t sa_family'' of struct sockaddr; this, in turn, causes   problems when sockaddr_s objects bogusly used as sockaddr ones, e.g.:
    connect(4, { sa_len = 2, sa_family = 0, ...) ERR#47 'Address family
    not supported by protocol family'

- Fix missing main window icon by calling pixbuf_new_from_file_with_mask()
  with correct filepath
- Since these changes are likely to improve GtkRadiant user's experience,
  tentatively bump port revision
- Fix comment indentation in `radiant/treemodel.cpp' (broken in r384527)

games/gtkradiant/Makefile
@@ -3,7 +3,7 @@
 
 PORTNAME=	gtkradiant
 PORTVERSION=	1.5.0
-PORTREVISION=	13
+PORTREVISION=	14
 CATEGORIES=	games cad
 MASTER_SITES=	IDSOFTWARE/source/:id \
 		http://www.bsd-geek.de/FreeBSD/distfiles/:fb

games/gtkradiant/files/patch-libs_l__net_l__net__berkley.c
@@ -1,0 +1,29 @@
+--- libs/l_net/l_net_berkley.c.orig	2006-02-10 22:01:20 UTC
++++ libs/l_net/l_net_berkley.c
+@@ -438,7 +438,7 @@ static int PartialIPAddress (char *in, s
+ 		addr = (addr<<8) + num;
+ 	}
+ 	
+-	hostaddr->sa_family = AF_INET;
++	((struct sockaddr_in *)hostaddr)->sin_family = AF_INET;
+ 	((struct sockaddr_in *)hostaddr)->sin_port = htons((u_short)net_hostport);
+ 	((struct sockaddr_in *)hostaddr)->sin_addr.s_addr = (myAddr & htonl(mask)) | htonl(addr);
+ 	
+@@ -657,7 +657,7 @@ int WINS_StringToAddr(char *string, stru
+ 	sscanf(string, "%d.%d.%d.%d:%d", &ha1, &ha2, &ha3, &ha4, &hp);
+ 	ipaddr = (ha1 << 24) | (ha2 << 16) | (ha3 << 8) | ha4;
+ 
+-	addr->sa_family = AF_INET;
++	((struct sockaddr_in *)addr)->sin_family = AF_INET;
+ 	((struct sockaddr_in *)addr)->sin_addr.s_addr = htonl(ipaddr);
+ 	((struct sockaddr_in *)addr)->sin_port = htons((u_short)hp);
+ 	return 0;
+@@ -718,7 +718,7 @@ int WINS_GetAddrFromName(char *name, str
+ 	if (!hostentry)
+ 		return -1;
+ 
+-	addr->sa_family = AF_INET;
++	((struct sockaddr_in *)addr)->sin_family = AF_INET;
+ 	((struct sockaddr_in *)addr)->sin_port = htons((u_short)net_hostport);
+ 	((struct sockaddr_in *)addr)->sin_addr.s_addr = *(int *)hostentry->h_addr_list[0];
+ 

games/gtkradiant/files/patch-radiant__mainframe.cpp
@@ -9,6 +9,15 @@
  "so"
  #elif defined (__APPLE__)
  "dylib"
+@@ -2849,7 +2849,7 @@
+ 
+ #if !defined(WIN32)
+   {
+-    GdkPixbuf* pixbuf = pixbuf_new_from_file_with_mask("icon.bmp");
++    GdkPixbuf* pixbuf = pixbuf_new_from_file_with_mask("bitmaps/icon.bmp");
+     if(pixbuf != 0)
+     {
+       gtk_window_set_icon(window, pixbuf);
 @@ -3479,7 +3479,7 @@
      const char* ENGINEPATH_ATTRIBUTE =
  #if defined(WIN32)

games/gtkradiant/files/patch-radiant_treemodel.cpp
@@ -20,7 +20,7 @@
  void node_detach_name_changed_callback(scene::Node& node, const NameCallback& callback)
  {
 -  if(&node != 0)
-+  volatile intptr_t n = (intptr_t)&node;		// see the comment on line 713
++  volatile intptr_t n = (intptr_t)&node;	// see the comment on line 713
 +
 +  if(n != 0)
    {
@@ -31,7 +31,7 @@
  const char* node_get_name_safe(scene::Node& node)
  {
 -  if(&node == 0)
-+  volatile intptr_t n = (intptr_t)&node;		// see the comment on line 713
++  volatile intptr_t n = (intptr_t)&node;	// see the comment on line 713
 +
 +  if(n == 0)
    {
@@ -42,7 +42,7 @@
  void node_attach_name_changed_callback(scene::Node& node, const NameCallback& callback)
  {
 -  if(&node != 0)
-+  volatile intptr_t n = (intptr_t)&node;		// see the comment on line 713
++  volatile intptr_t n = (intptr_t)&node;	// see the comment on line 713
 +
 +  if(n != 0)
    {
@@ -53,7 +53,7 @@
  void node_detach_name_changed_callback(scene::Node& node, const NameCallback& callback)
  {
 -  if(&node != 0)
-+  volatile intptr_t n = (intptr_t)&node;		// see the comment on line 713
++  volatile intptr_t n = (intptr_t)&node;	// see the comment on line 713
 +
 +  if(n != 0)
    {