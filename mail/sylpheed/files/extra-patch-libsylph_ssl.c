swills/freebsd-ports

commit dbe2d9b310ec1caaa77dc31b29a94c2e2aa6238c

Provide fix for sigbus on ARM caused by unaligned access.
PR:		200053
Submitted by:	mikael.urankar@gmail.com, usenet@ulrich-grey.de


git-svn-id: svn+ssh://svn.freebsd.org/ports/head@391392 35697150-7ecd-e111-bb59-0022644237b5

mail/sylpheed/Makefile
@@ -3,7 +3,7 @@
 
 PORTNAME=	sylpheed
 PORTVERSION=	3.4.2
-PORTREVISION=	2
+PORTREVISION=	3
 CATEGORIES=	mail ipv6
 MASTER_SITES=	http://sylpheed.sraoss.jp/sylpheed/v${PORTVERSION:C/\.[^\.]*$//}/  \
 		LOCAL/ehaupt

mail/sylpheed/files/patch-libsylph_procmsg.c
@@ -1,0 +1,20 @@
+--- libsylph/procmsg.c.orig	2014-06-10 04:06:35 UTC
++++ libsylph/procmsg.c
+@@ -164,7 +164,7 @@ static gint procmsg_read_cache_data_str_
+ 	if (endp - *p < sizeof(len))
+ 		return -1;
+ 
+-	len = *(const guint32 *)(*p);
++	memcpy(&len, *p, sizeof(len));
+ 	*p += sizeof(len);
+ 	if (len > G_MAXINT || len > endp - *p)
+ 		return -1;
+@@ -197,7 +197,7 @@ static gint procmsg_read_cache_data_str_
+ 		g_mapped_file_free(mapfile);			\
+ 		return NULL;					\
+ 	} else {						\
+-		n = *(const guint32 *)p;			\
++		memcpy(&n, p, sizeof(n));			\
+ 		p += sizeof(guint32);				\
+ 	}							\
+ }

commit 316239135a1d16bd245bce57049cd3a9379aeb5d

Updated patch to fix breakage on platforms with a 64 bit time_t. This fixes a regression that was introduced with r391392.
PR:		202832
Submitted by:	yamagi@yamagi.org

mail/sylpheed/Makefile
@@ -3,6 +3,7 @@
 
 PORTNAME=	sylpheed
 PORTVERSION=	3.4.3
+PORTREVISION=	1
 CATEGORIES=	mail ipv6
 MASTER_SITES=	http://sylpheed.sraoss.jp/sylpheed/v${PORTVERSION:C/\.[^\.]*$//}/  \
 		LOCAL/ehaupt

mail/sylpheed/files/extra-patch-libsylph_ssl.c
@@ -1,6 +1,6 @@
---- libsylph/ssl.c.orig	2014-06-10 06:06:36.000000000 +0200
-+++ libsylph/ssl.c	2015-02-16 16:38:30.082429979 +0100
-@@ -59,6 +59,7 @@
+--- libsylph/ssl.c.orig	2014-06-10 04:06:36 UTC
++++ libsylph/ssl.c
+@@ -59,6 +59,7 @@ static gchar *find_certs_file(const gcha
  		LOOK_FOR("ca-bundle.crt");
  		LOOK_FOR("ca-root.crt");
  		LOOK_FOR("certs.crt");
@@ -8,7 +8,7 @@
  	}
  
  #undef LOOK_FOR
-@@ -117,7 +118,7 @@
+@@ -117,7 +118,7 @@ void ssl_init(void)
  			certs_dir = NULL;
  		}
  #else

mail/sylpheed/files/patch-libsylph-defs.h
@@ -1,6 +1,6 @@
---- libsylph/defs.h.orig	Tue Sep 13 16:55:49 2005
-+++ libsylph/defs.h	Mon Sep 19 03:23:15 2005
-@@ -81,8 +81,8 @@
+--- libsylph/defs.h.orig	2014-06-10 04:06:36 UTC
++++ libsylph/defs.h
+@@ -89,8 +89,8 @@
  #else
  #  define DEFAULT_SIGNATURE	".signature"
  #endif
@@ -11,7 +11,7 @@
  /* #define DEFAULT_INC_PATH	"/usr/bin/imget" */
  /* #define DEFAULT_INC_PROGRAM	"imget" */
  #define DEFAULT_SENDMAIL_CMD	"/usr/sbin/sendmail -t -i"
-@@ -91,7 +91,7 @@
+@@ -103,7 +103,7 @@
  #ifdef _PATH_MAILDIR
  #  define DEFAULT_SPOOL_PATH	_PATH_MAILDIR
  #else

mail/sylpheed/files/patch-libsylph_procmsg.c
@@ -9,12 +9,14 @@
  	*p += sizeof(len);
  	if (len > G_MAXINT || len > endp - *p)
  		return -1;
-@@ -197,7 +197,7 @@ static gint procmsg_read_cache_data_str_
+@@ -197,7 +197,9 @@ static gint procmsg_read_cache_data_str_
  		g_mapped_file_free(mapfile);			\
  		return NULL;					\
  	} else {						\
 -		n = *(const guint32 *)p;			\
-+		memcpy(&n, p, sizeof(n));			\
++		guint32 v;					\
++		memcpy(&v, p, sizeof(v));			\
++		n = v;						\
  		p += sizeof(guint32);				\
  	}							\
  }

mail/sylpheed/files/patch-src-printing.c
@@ -1,6 +1,6 @@
---- src/printing.c.orig	Wed Dec 27 15:50:14 2006
-+++ src/printing.c	Wed Dec 27 16:28:28 2006
-@@ -149,8 +149,6 @@
+--- src/printing.c.orig	2014-06-10 04:06:36 UTC
++++ src/printing.c
+@@ -151,8 +151,6 @@ static gint layout_set_headers(PangoLayo
  
  	desc = pango_font_description_from_string(prefs_common_get()->textfont);
  	size = pango_font_description_get_size(desc);
@@ -9,9 +9,9 @@
  	pango_font_description_set_size(desc, size);
  	pango_layout_set_font_description(layout, desc);
  	pango_font_description_free(desc);
-@@ -406,10 +404,6 @@
+@@ -417,10 +415,6 @@ static void draw_page(GtkPrintOperation 
  	}
- 	g_print("count = %d\n", count);
+ 	debug_print("count = %d\n", count);
  
 -	desc = gtkut_get_default_font_desc();
 -	pango_font_description_set_size(desc, font_size);