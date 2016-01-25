swills/freebsd-ports

commit 2caff06ade716a519616fdcaffc968cb760b74e6

- Fix build with clang - Remove leading article from COMMENT - Convert to new LIB_DEPENDS format - Support staging
While I'm here use option helpers where possible.

PR:		ports/184816
Submitted by:	KATO Tsuguru <tkato432@yahoo.com>


git-svn-id: svn+ssh://svn.freebsd.org/ports/head@336927 35697150-7ecd-e111-bb59-0022644237b5

irc/minbif/Makefile
@@ -8,11 +8,11 @@ CATEGORIES=	irc net-im
 MASTER_SITES=	https://symlink.me/attachments/download/148/
 
 MAINTAINER=	ports@FreeBSD.org
-COMMENT=	An IRC to instant messaging gateway
+COMMENT=	IRC to instant messaging gateway
 
 LICENSE=	GPLv2
 
-LIB_DEPENDS=	purple:${PORTSDIR}/net-im/libpurple
+LIB_DEPENDS=	libpurple.so:${PORTSDIR}/net-im/libpurple
 
 # right now PLUGINS installs prpl-coincoin and prpl-gayattitude
 OPTIONS_DEFINE=	CACA GNUTLS VIDEO PLUGINS DOCS
@@ -22,13 +22,14 @@ VIDEO_DESC=	Webcam display using libcaca (depends on CACA)
 PLUGINS_DESC=	Compile additional libpurple-plugins
 
 USE_RC_SUBR=	minbif
+
+USES=		cmake pkgconfig
 USE_GNOME=	glib20
 USE_EFL=	imlib2
-USES=		cmake pkgconfig
 CMAKE_ARGS=	-DENABLE_IMLIB:BOOL=ON
 
-CFLAGS+=	${PTHREAD_CFLAGS} -DX_DISPLAY_MISSING
-LDFLAGS+=	${PTHREAD_LIBS}
+CFLAGS+=	-DX_DISPLAY_MISSING
+LDFLAGS+=	-pthread
 
 USERS=		minbif
 GROUPS=		minbif
@@ -36,25 +37,30 @@ GROUPS=		minbif
 SUB_FILES=	pkg-message
 SUB_LIST=	USERS="${USERS}"
 
-MAN8=		minbif.8
 PORTDOCS=	*
 PLIST_FILES=	bin/minbif %%ETCDIR%%/minbif.conf-dist \
-		%%ETCDIR%%/minbif.motd-dist
+		%%ETCDIR%%/minbif.motd-dist man/man8/minbif.8.gz
 PLIST_DIRS=	%%ETCDIR%%
 
-NO_STAGE=	yes
-.include <bsd.port.options.mk>
+CACA_LIB_DEPENDS=	libcaca.so:${PORTSDIR}/graphics/libcaca
+CACA_CMAKE_ON=		-DENABLE_CACA:BOOL=ON
+CACA_CMAKE_OFF=		-DENABLE_CACA:BOOL=OFF
 
-.if ${PORT_OPTIONS:MCACA}
-LIB_DEPENDS+=	caca:${PORTSDIR}/graphics/libcaca
-CMAKE_ARGS+=	-DENABLE_CACA:BOOL=ON
-.else
-CMAKE_ARGS+=	-DENABLE_CACA:BOOL=OFF
-.endif
+GNUTLS_LIB_DEPENDS=	libgnutls.so:${PORTSDIR}/security/gnutls
+GNUTLS_CMAKE_ON=	-DENABLE_TLS:BOOL=ON
+GNUTLS_CMAKE_OFF=	-DENABLE_TLS:BOOL=OFF
+
+PLUGINS_USE=		GNOME=libxml2
+PLUGINS_CMAKE_ON=	-DENABLE_PLUGIN:BOOL=ON
+PLUGINS_CMAKE_OFF=	-DENABLE_PLUGIN:BOOL=OFF
+PLUGINS_PLIST_FILES=	lib/purple-2/libcoincoin.so \
+			lib/purple-2/libgayattitude.so
+
+.include <bsd.port.options.mk>
 
 .if ${PORT_OPTIONS:MVIDEO} && !empty(PORT_OPTIONS:MCACA)
 USE_GSTREAMER+=	core good
-LIB_DEPENDS+=	gstfarsight-0.10:${PORTSDIR}/net-im/farsight2
+LIB_DEPENDS+=	libgstfarsight-0.10.so:${PORTSDIR}/net-im/farsight2
 CMAKE_ARGS+=	-DENABLE_VIDEO:BOOL=ON
 .else
 CMAKE_ARGS+=	-DENABLE_VIDEO:BOOL=OFF
@@ -65,46 +71,37 @@ IGNORE=		if you want to enable webcam support, you must \
 		also enable CACA.
 .endif
 
-.if ${PORT_OPTIONS:MPLUGINS}
-USE_GNOME+=	libxml2
-CMAKE_ARGS+=	-DENABLE_PLUGIN:BOOL=ON
-PLIST_FILES+=	lib/purple-2/libcoincoin.so \
-		lib/purple-2/libgayattitude.so
-.else
-CMAKE_ARGS+=	-DENABLE_PLUGIN:BOOL=OFF
-.endif
-
-.if ${PORT_OPTIONS:MGNUTLS}
-LIB_DEPENDS+=	gnutls:${PORTSDIR}/security/gnutls
-CMAKE_ARGS+=	-DENABLE_TLS:BOOL=ON
-.else
-CMAKE_ARGS+=	-DENABLE_TLS:BOOL=OFF
-.endif
-
 post-patch:
 	@${REINPLACE_CMD} -e \
 		's|"-lpthread -lstdc++"||' ${WRKSRC}/src/CMakeLists.txt
 	@${REINPLACE_CMD} -e \
 		'/pam_misc.h/d' ${WRKSRC}/src/im/auth_pam.h
-	@${REINPLACE_CMD} -e 's|glib/.*\.h>|glib.h>|g' \
-		${WRKSRC}/src/server_poll/daemon_fork.cpp \
-		${WRKSRC}/src/server_poll/inetd.cpp
+
+post-build:
+	@${LN} -sf minbif.conf ${WRKSRC}/minbif.conf-dist
+	@${LN} -sf minbif.motd ${WRKSRC}/minbif.motd-dist
 
 do-install:
-	${INSTALL_PROGRAM} ${WRKSRC}/src/${PORTNAME} ${PREFIX}/bin
-	${INSTALL_MAN} ${WRKSRC}/man/minbif.8 ${MAN8PREFIX}/man/man8
-	${MKDIR} ${ETCDIR}
-	${INSTALL_DATA} ${WRKSRC}/minbif.conf ${ETCDIR}/minbif.conf-dist
-	${INSTALL_DATA} ${WRKSRC}/minbif.motd ${ETCDIR}/minbif.motd-dist
+	(cd ${WRKSRC}/src && ${INSTALL_PROGRAM} ${PORTNAME} \
+		${STAGEDIR}${PREFIX}/bin)
+	(cd ${WRKSRC}/man && ${INSTALL_MAN} minbif.8 \
+		${STAGEDIR}${MAN8PREFIX}/man/man8)
+	@${MKDIR} ${STAGEDIR}${ETCDIR}
+	(cd ${WRKSRC} && ${INSTALL_DATA} minbif.conf-dist \
+		${STAGEDIR}${ETCDIR})
+	(cd ${WRKSRC} && ${INSTALL_DATA} minbif.motd-dist \
+		${STAGEDIR}${ETCDIR})
 .if ${PORT_OPTIONS:MPLUGINS}
-	${INSTALL_PROGRAM} ${WRKSRC}/plugins/coincoin/libcoincoin.so ${PREFIX}/lib/purple-2
-	${INSTALL_PROGRAM} ${WRKSRC}/plugins/gayattitude/libgayattitude.so ${PREFIX}/lib/purple-2
+	@${MKDIR} ${STAGEDIR}${PREFIX}/lib/purple-2
+	(cd ${WRKSRC}/plugins/coincoin && ${INSTALL_LIB} libcoincoin.so \
+		${STAGEDIR}${PREFIX}/lib/purple-2)
+	(cd ${WRKSRC}/plugins/gayattitude && ${INSTALL_LIB} libgayattitude.so \
+		${STAGEDIR}${PREFIX}/lib/purple-2)
 .endif
-.if ${PORT_OPTIONS:MDOCS}
-	${MKDIR} ${DOCSDIR}
-	${INSTALL_DATA} ${WRKSRC}/README ${DOCSDIR}
-	${INSTALL_DATA} ${WRKSRC}/doc/minbif.xinetd ${DOCSDIR}
-.endif
-	@${CAT} ${PKGMESSAGE}
+	@${MKDIR} ${STAGEDIR}${DOCSDIR}
+	(cd ${WRKSRC} && ${INSTALL_DATA} README \
+		${STAGEDIR}${DOCSDIR})
+	(cd ${WRKSRC}/doc && ${INSTALL_DATA} minbif.xinetd \
+		${STAGEDIR}${DOCSDIR})
 
 .include <bsd.port.mk>

irc/minbif/files/patch-src__im__account.cpp
@@ -1,0 +1,10 @@
+--- src/im/account.cpp.orig
++++ src/im/account.cpp
+@@ -18,6 +18,7 @@
+ 
+ #include <cassert>
+ #include <cstring>
++#include <unistd.h>
+ #ifdef HAVE_IMLIB
+ 	#include <Imlib2.h>
+ #endif /* HAVE_IMLIB */

irc/minbif/files/patch-src__core__minbif.cpp
@@ -1,0 +1,21 @@
+--- src/core/minbif.cpp.orig
++++ src/core/minbif.cpp
+@@ -17,7 +17,7 @@
+  */
+ 
+ #include <cerrno>
+-#include <stdlib.h>
++#include <cstdlib>
+ #include <iostream>
+ #include <fstream>
+ #include <cstring>
+@@ -239,7 +239,9 @@
+ 		}
+ 		sighandler.setApplication(this);
+ 
++#if ! GLIB_CHECK_VERSION(2, 32, 0)
+ 		g_thread_init(NULL);
++#endif
+ 		loop = g_main_new(FALSE);
+ 		g_main_run(loop);
+ 

irc/minbif/files/patch-src__im__auth_pam.cpp
@@ -1,0 +1,10 @@
+--- src/im/auth_pam.cpp.orig
++++ src/im/auth_pam.cpp
+@@ -19,6 +19,7 @@
+ #include <cstring>
+ #include <cerrno>
+ #include <sys/types.h>
++#include <unistd.h>
+ #include <pwd.h>
+ 
+ #include "auth.h"

irc/minbif/files/patch-src__im__request.cpp
@@ -1,0 +1,14 @@
+--- src/im/request.cpp.orig
++++ src/im/request.cpp
+@@ -16,8 +16,9 @@
+  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+  */
+ 
+-#include <errno.h>
+-#include <string.h>
++#include <cerrno>
++#include <cstring>
++#include <unistd.h>
+ 
+ #include "request.h"
+ #include "buddy.h"

irc/minbif/files/patch-src__irc__dcc.cpp
@@ -1,0 +1,19 @@
+--- src/irc/dcc.cpp.orig
++++ src/irc/dcc.cpp
+@@ -17,12 +17,13 @@
+ 
+ #include <sys/types.h>
+ #include <sys/socket.h>
++#include <netinet/in.h>
+ #include <arpa/inet.h>
+ #include <fcntl.h>
+-#include <errno.h>
+-#include <string.h>
++#include <cerrno>
++#include <cstring>
+ #include <sys/types.h>
+-#include <netinet/in.h>
++#include <unistd.h>
+ 
+ #include "dcc.h"
+ #include "nick.h"

irc/minbif/files/patch-src__server_poll__daemon_fork.cpp
@@ -1,0 +1,17 @@
+--- src/server_poll/daemon_fork.cpp.orig
++++ src/server_poll/daemon_fork.cpp
+@@ -20,10 +20,12 @@
+ #include <cassert>
+ #include <cstring>
+ #include <cerrno>
+-#include <glib/gmain.h>
++#include <glib.h>
++#include <sys/types.h>
+ #include <sys/socket.h>
+-#include <sys/stat.h>
++#include <netinet/in.h>
+ #include <arpa/inet.h>
++#include <sys/stat.h>
+ 
+ #include "daemon_fork.h"
+ #include "irc/irc.h"

irc/minbif/files/patch-src__server_poll__inetd.cpp
@@ -1,0 +1,12 @@
+--- src/server_poll/inetd.cpp.orig
++++ src/server_poll/inetd.cpp
+@@ -17,7 +17,8 @@
+  */
+ 
+ #include <cassert>
+-#include <glib/gmain.h>
++#include <unistd.h>
++#include <glib.h>
+ 
+ #include "inetd.h"
+ #include "irc/irc.h"

commit 11a5eeb58d5110359d32efbbafb34daa7b201fd9

- Unbreak the build against new Imlib2: previous versions (e.g. 1.4.6) had   two typedefs (apparently, the latter was deprecated and finally removed   in 1.4.7):
    typedef enum _imlib_load_error Imlib_Load_Error;
    typedef enum _imlib_load_error ImlibLoadError;

- Instead of handling of VIDEO and CACA options interdependency manually,
  define VIDEO_IMPLIES=CACA (available since r394573) and thus consummate
  conversion to option helpers
- Install sample configuration files as @sample in pkg-plist (and convert
  PLIST_FILES to separate file due to better readability thereof)
- Restore sane installation routine broken in r336927 and install couple
  of extra documentation files while here
- Augment port description text and update WWW line to avoid redirect

irc/minbif/Makefile
@@ -3,7 +3,7 @@
 
 PORTNAME=	minbif
 PORTVERSION=	1.0.5
-PORTREVISION=	7
+PORTREVISION=	8
 CATEGORIES=	irc net-im
 MASTER_SITES=	https://symlink.me/attachments/download/148/
 
@@ -12,21 +12,11 @@ COMMENT=	IRC to instant messaging gateway
 
 LICENSE=	GPLv2
 
-BROKEN=		fails to build
-
 LIB_DEPENDS=	libpurple.so:${PORTSDIR}/net-im/libpurple
-
-# right now PLUGINS installs prpl-coincoin and prpl-gayattitude
-OPTIONS_DEFINE=	CACA GNUTLS VIDEO PLUGINS DOCS
-OPTIONS_DEFAULT=	CACA
-CACA_DESC=	Buddy icon display using libcaca
-VIDEO_DESC=	Webcam display using libcaca (depends on CACA)
-PLUGINS_DESC=	Compile additional libpurple-plugins
-
-USE_RC_SUBR=	minbif
 
 USES=		cmake pkgconfig
 USE_GNOME=	glib20
+USE_RC_SUBR=	${PORTNAME}
 
 CFLAGS+=	-DX_DISPLAY_MISSING
 LDFLAGS+=	-pthread
@@ -37,11 +27,16 @@ GROUPS=		minbif
 SUB_FILES=	pkg-message
 SUB_LIST=	USERS="${USERS}"
 
-PORTDOCS=	*
-PLIST_FILES=	bin/minbif %%ETCDIR%%/minbif.conf-dist \
-		%%ETCDIR%%/minbif.motd-dist man/man8/minbif.8.gz
+OPTIONS_DEFINE=	CACA GNUTLS VIDEO PLUGINS DOCS
+OPTIONS_DEFAULT=	CACA
+VIDEO_IMPLIES=	CACA
 
-CACA_LIB_DEPENDS=	libcaca.so:${PORTSDIR}/graphics/libcaca
+CACA_DESC=	Buddy icon display using libcaca
+VIDEO_DESC=	Webcam display using libcaca (implies CACA)
+PLUGINS_DESC=	Compile additional libpurple-plugins
+
+CACA_LIB_DEPENDS=	libcaca.so:${PORTSDIR}/graphics/libcaca \
+			libImlib2.so:${PORTSDIR}/graphics/imlib2
 CACA_CMAKE_ON=		-DENABLE_CACA:BOOL=ON -DENABLE_IMLIB:BOOL=ON
 CACA_CMAKE_OFF=		-DENABLE_CACA:BOOL=OFF -DENABLE_IMLIB:BOOL=OFF
 
@@ -55,55 +50,35 @@ PLUGINS_CMAKE_OFF=	-DENABLE_PLUGIN:BOOL=OFF
 PLUGINS_PLIST_FILES=	lib/purple-2/libcoincoin.so \
 			lib/purple-2/libgayattitude.so
 
-.include <bsd.port.options.mk>
-
-.if ${PORT_OPTIONS:MVIDEO} && !empty(PORT_OPTIONS:MCACA)
-USE_GSTREAMER+=	core good
-LIB_DEPENDS+=	libgstfarsight-0.10.so:${PORTSDIR}/net-im/farsight2
-CMAKE_ARGS+=	-DENABLE_VIDEO:BOOL=ON
-.else
-CMAKE_ARGS+=	-DENABLE_VIDEO:BOOL=OFF
-.endif
-
-.if ${PORT_OPTIONS:MCACA}
-LIB_DEPENDS+=	libImlib2.so:${PORTSDIR}/graphics/imlib2
-.endif
-
-.if ${PORT_OPTIONS:MVIDEO} && empty(PORT_OPTIONS:MCACA)
-IGNORE=	if you want to enable webcam support, you must also enable CACA
-.endif
+VIDEO_USE=		GSTREAMER=core,good
+VIDEO_CMAKE_ON=		-DENABLE_VIDEO:BOOL=ON
+VIDEO_CMAKE_OFF=	-DENABLE_VIDEO:BOOL=OFF
+VIDEO_LIB_DEPENDS=	libgstfarsight-0.10.so:${PORTSDIR}/net-im/farsight2
 
 post-patch:
 	@${REINPLACE_CMD} -e \
 		's|"-lpthread -lstdc++"||' ${WRKSRC}/src/CMakeLists.txt
-	@${REINPLACE_CMD} -e \
-		'/pam_misc.h/d' ${WRKSRC}/src/im/auth_pam.h
-
-post-build:
-	@${LN} -sf minbif.conf ${WRKSRC}/minbif.conf-dist
-	@${LN} -sf minbif.motd ${WRKSRC}/minbif.motd-dist
+	@${REINPLACE_CMD} -e '/pam_misc\.h/d' ${WRKSRC}/src/im/auth_pam.h
 
 do-install:
-	(cd ${WRKSRC}/src && ${INSTALL_PROGRAM} ${PORTNAME} \
-		${STAGEDIR}${PREFIX}/bin)
-	(cd ${WRKSRC}/man && ${INSTALL_MAN} minbif.8 \
-		${STAGEDIR}${MAN8PREFIX}/man/man8)
+	${INSTALL_PROGRAM} ${WRKSRC}/src/${PORTNAME} ${STAGEDIR}${PREFIX}/bin
+	${INSTALL_MAN} ${WRKSRC}/man/minbif.8 ${STAGEDIR}${MANPREFIX}/man/man8
 	@${MKDIR} ${STAGEDIR}${ETCDIR}
-	(cd ${WRKSRC} && ${INSTALL_DATA} minbif.conf-dist \
-		${STAGEDIR}${ETCDIR})
-	(cd ${WRKSRC} && ${INSTALL_DATA} minbif.motd-dist \
-		${STAGEDIR}${ETCDIR})
-.if ${PORT_OPTIONS:MPLUGINS}
+	${INSTALL_DATA} ${WRKSRC}/minbif.conf \
+		${STAGEDIR}${ETCDIR}/minbif.conf.sample
+	${INSTALL_DATA} ${WRKSRC}/minbif.motd \
+		${STAGEDIR}${ETCDIR}/minbif.motd.sample
+
+do-install-PLUGINS-on:
 	@${MKDIR} ${STAGEDIR}${PREFIX}/lib/purple-2
-	(cd ${WRKSRC}/plugins/coincoin && ${INSTALL_LIB} libcoincoin.so \
-		${STAGEDIR}${PREFIX}/lib/purple-2)
-	(cd ${WRKSRC}/plugins/gayattitude && ${INSTALL_LIB} libgayattitude.so \
-		${STAGEDIR}${PREFIX}/lib/purple-2)
-.endif
+	${INSTALL_LIB} ${WRKSRC}/plugins/coincoin/libcoincoin.so \
+		${WRKSRC}/plugins/gayattitude/libgayattitude.so \
+			${STAGEDIR}${PREFIX}/lib/purple-2
+
+do-install-DOCS-on:
 	@${MKDIR} ${STAGEDIR}${DOCSDIR}
-	(cd ${WRKSRC} && ${INSTALL_DATA} README \
-		${STAGEDIR}${DOCSDIR})
-	(cd ${WRKSRC}/doc && ${INSTALL_DATA} minbif.xinetd \
-		${STAGEDIR}${DOCSDIR})
+.for f in AUTHORS ChangeLog README doc/minbif.xinetd
+	${INSTALL_DATA} ${WRKSRC}/${f} ${STAGEDIR}${DOCSDIR}
+.endfor
 
 .include <bsd.port.mk>

irc/minbif/files/patch-src__im__account.cpp
@@ -1,4 +1,4 @@
---- src/im/account.cpp.orig
+--- src/im/account.cpp.orig	2011-12-04 14:24:51 UTC
 +++ src/im/account.cpp
 @@ -18,6 +18,7 @@
  
@@ -8,3 +8,12 @@
  #ifdef HAVE_IMLIB
  	#include <Imlib2.h>
  #endif /* HAVE_IMLIB */
+@@ -272,7 +273,7 @@ void Account::setBuddyIcon(string filena
+ 				else
+ 				{
+ 					char** prpl_formats = g_strsplit(prplinfo->icon_spec.format,",",0);
+-					ImlibLoadError err = IMLIB_LOAD_ERROR_UNKNOWN;
++					Imlib_Load_Error err = IMLIB_LOAD_ERROR_UNKNOWN;
+ 
+ 					close(temp_fd);
+ 					/* Try to encode in a supported format. */

irc/minbif/pkg-descr
@@ -1,4 +1,7 @@
-Minbif aims to use the libpurple library from the Pidgin project
-to provide an IRC-friendly instant messaging client.
+Minbif aims to use the libpurple library from the Pidgin project to provide
+an IRC-friendly instant messaging client.
 
-WWW: http://minbif.im
+It is very easy to setup, does not require any non-standard authentication,
+and just uses the server password feature of IRC clients.
+
+https://symlink.me/projects/minbif/wiki/

irc/minbif/pkg-plist
@@ -1,0 +1,8 @@
+bin/minbif
+@sample %%ETCDIR%%/minbif.conf.sample
+@sample %%ETCDIR%%/minbif.motd.sample
+man/man8/minbif.8.gz
+%%PORTDOCS%%%%DOCSDIR%%/AUTHORS
+%%PORTDOCS%%%%DOCSDIR%%/ChangeLog
+%%PORTDOCS%%%%DOCSDIR%%/README
+%%PORTDOCS%%%%DOCSDIR%%/minbif.xinetd