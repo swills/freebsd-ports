fa597fd0b42b7d618ea9722890c1c641550bad93
Steve Wills <steve@mouf.net>
2018-02-27 14:14:50 -0500

    Use gitlab-runner ports tree

--- gitlab-build.sh
+++ gitlab-build.sh
@@ -29,4 +29,4 @@ www/nginx@all
 www/rubygem-selenium-webdriver@all
 EOF
 
-sudo nice -n 18 /usr/sbin/idprio 29 poudriere bulk -t -B ${CI_JOB_ID} -j 111-amd64 -p swills-git-jenkins -f ${PORT_LIST}
+sudo nice -n 18 /usr/sbin/idprio 29 poudriere bulk -t -B ${CI_JOB_ID} -j 111-amd64 -p ${CI_PROJECT_NAMESPACE}-${CI_PROJECT_NAME} -f ${PORT_LIST}
