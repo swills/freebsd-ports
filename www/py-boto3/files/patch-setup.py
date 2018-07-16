15adad37ac0bf92cdba03f6af294fd9af6e77582
Steve Wills <steve@mouf.net>
2018-07-16 14:52:34 -0400

    www/py-boto3:
    PR:		227656
    Submitted by:
    Reported by:
    Reviewed by:
    Approved by:
    Obtained from:
    MFC after:
    MFH:
    Relnotes:
    Security:
    Sponsored by:
    Differential Revision:
    #
    # Description of fields to fill in above:                     76 columns --|
    # PR:                       If and which Problem Report is related.
    # Submitted by:             If someone else sent in the change.
    # Reported by:              If someone else reported the issue.
    # Reviewed by:              If someone else reviewed your modification.
    # Approved by:              If you needed approval for this commit.
    # Obtained from:            If the change is from a third party.
    # MFC after:                N [day[s]|week[s]|month[s]].  Request a reminder email.
    # MFH:                      Ports tree branch name.  Request approval for merge.
    # Relnotes:                 Set to 'yes' for mention in release notes.
    # Security:                 Vulnerability reference (one per line) or description.
    # Sponsored by:             If the change was sponsored by an organization.
    # Differential Revision:    https://reviews.freebsd.org/D### (*full* phabric URL needed).
    #
    # See 'Commit Log Messages' in the Committer's Guide for details.
    #
    # -------------------------------------------------------------------------
    # To use this template, run this command from the top of the ports tree.
    #
    #	git config commit.template .gitmessage
    # -------------------------------------------------------------------------

--- www/py-boto3/files/patch-setup.py
+++ www/py-boto3/files/patch-setup.py
@@ -1,15 +1,15 @@
---- setup.py.orig	2016-08-03 00:15:58 UTC
+--- setup.py.orig	2018-04-20 14:22:59 UTC
 +++ setup.py
-@@ -15,11 +15,14 @@ VERSION_RE = re.compile(r'''__version__ 
+@@ -14,11 +14,14 @@ VERSION_RE = re.compile(r'''__version__ 
  
  
  requires = [
--    'botocore>=1.4.1,<1.5.0',
+-    'botocore>=1.10.5,<1.11.0',
 -    'jmespath>=0.7.1,<1.0.0',
--    's3transfer>=0.1.0,<0.2.0'
-+    'botocore>=1.4.1',
+-    's3transfer>=0.1.10,<0.2.0'
++    'botocore>=1.10.5',
 +    'jmespath>=0.7.1',
-+    's3transfer>=0.1.0'
++    's3transfer>=0.1.10'
  ]
  
 +tests_require = [
@@ -18,7 +18,7 @@
  
  def get_version():
      init = open(os.path.join(ROOT, 'boto3', '__init__.py')).read()
-@@ -43,6 +46,8 @@ setup(
+@@ -42,6 +45,8 @@ setup(
      },
      include_package_data=True,
      install_requires=requires,
