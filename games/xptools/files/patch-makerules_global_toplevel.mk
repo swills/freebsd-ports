a189be703b5d63dd78189713fcefbde1096839df
Steve Wills <steve@mouf.net>
2018-11-05 14:00:36 -0500

    games/xptools:
    PR:		232999
    Submitted by:
    Reported by:
    Reviewed by:
    Approved by:
    Obtained from:
    MFC after:
    MFH:
    Relnotes:
    Security:
    Sponsored by:
    Differential Revision:
    #
    # Description of fields to fill in above:                     76 columns --|
    # PR:                       If and which Problem Report is related.
    # Submitted by:             If someone else sent in the change.
    # Reported by:              If someone else reported the issue.
    # Reviewed by:              If someone else reviewed your modification.
    # Approved by:              If you needed approval for this commit.
    # Obtained from:            If the change is from a third party.
    # MFC after:                N [day[s]|week[s]|month[s]].  Request a reminder email.
    # MFH:                      Ports tree branch name.  Request approval for merge.
    # Relnotes:                 Set to 'yes' for mention in release notes.
    # Security:                 Vulnerability reference (one per line) or description.
    # Sponsored by:             If the change was sponsored by an organization.
    # Differential Revision:    https://reviews.freebsd.org/D### (*full* phabric URL needed).
    #
    # See 'Commit Log Messages' in the Committer's Guide for details.
    #
    # -------------------------------------------------------------------------
    # To use this template, run this command from the top of the ports tree.
    #
    #	git config commit.template .gitmessage
    # -------------------------------------------------------------------------

--- games/xptools/files/patch-makerules_global_toplevel.mk
+++ games/xptools/files/patch-makerules_global_toplevel.mk
@@ -1,34 +1,15 @@
---- makerules/global/toplevel.mk.orig	2017-04-14 14:21:02 UTC
+--- makerules/global/toplevel.mk.orig	2018-11-05 18:41:03 UTC
 +++ makerules/global/toplevel.mk
-@@ -8,7 +8,7 @@
- .PHONY: all clean linkclean
- 
- PLATFORM	:= $(shell uname)
--ARCHITECTURE	:= $(shell uname -m)
-+ARCHITECTURE	:= $(shell uname -m |sed s/amd64/x86_64/)
- WD		:= $(PWD)
- 
- 
-@@ -23,6 +23,9 @@
- ifeq ($(PLATFORM), Linux)
- 	PLAT_LINUX	:= Yes
- endif
-+ifeq ($(PLATFORM), FreeBSD)
-+	PLAT_LINUX	:= Yes
-+endif
- ifeq ($(PLATFORM), Darwin)
- 	PLAT_DARWIN	:= Yes
- endif
-@@ -110,9 +113,9 @@
+@@ -109,9 +109,9 @@
  # if someone has a ppc linux machine, please define -DLIL/-DBIG in the code,
  # remove them here and use the __ppc__ macro to resolve endianess issues
  	DEFINES		:= -DLIN=1 -DIBM=0 -DAPL=0 -DLIL=1 -DBIG=0
--	CFLAGS		:= $(M32_SWITCH) -Wno-deprecated-declarations -Wno-multichar -pipe -frounding-math
--	CXXFLAGS	:= $(M32_SWITCH) -Wno-deprecated -Wno-deprecated-declarations -Wno-multichar -pipe -frounding-math
--	LDFLAGS		:= $(M32_SWITCH) -static-libgcc
-+	CFLAGS		:= $(M32_SWITCH) -Wno-deprecated-declarations -Wno-multichar -pipe
-+	CXXFLAGS	:= $(M32_SWITCH) -Wno-deprecated -Wno-deprecated-declarations -Wno-multichar -pipe
-+	LDFLAGS		:= $(M32_SWITCH)
+-	CFLAGS		:= $(M32_SWITCH) -D_GLIBCXX_USE_CXX11_ABI=0 -Wno-deprecated-declarations -Wno-multichar -pipe -frounding-math
+-	CXXFLAGS	:= $(M32_SWITCH) -D_GLIBCXX_USE_CXX11_ABI=0 -Wno-deprecated -Wno-deprecated-declarations -Wno-multichar -pipe -frounding-math
+-	LDFLAGS		:= $(M32_SWITCH) -D_GLIBCXX_USE_CXX11_ABI=0 -static-libgcc
++	CFLAGS		:= $(M32_SWITCH) -D_GLIBCXX_USE_CXX11_ABI=0 -Wno-deprecated-declarations -Wno-multichar -pipe
++	CXXFLAGS	:= $(M32_SWITCH) -D_GLIBCXX_USE_CXX11_ABI=0 -Wno-deprecated -Wno-deprecated-declarations -Wno-multichar -pipe
++	LDFLAGS		:= $(M32_SWITCH) -D_GLIBCXX_USE_CXX11_ABI=0
  	BARE_LDFLAGS	:=
  	STRIPFLAGS	:= -s -x
  endif
