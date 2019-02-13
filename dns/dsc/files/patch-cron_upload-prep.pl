b5ea4ebce2176cc1d9f39eaee45b61f9d44a5039
Steve Wills <steve@mouf.net>
2019-02-13 05:15:39 -0500

    dns/dsc:
    PR:		235711
    Submitted by:
    Reported by:
    Reviewed by:
    Approved by:
    Obtained from:
    MFC after:
    MFH:
    Relnotes:
    Security:
    Sponsored by:
    Differential Revision:
    #
    # Description of fields to fill in above:                     76 columns --|
    # PR:                       If and which Problem Report is related.
    # Submitted by:             If someone else sent in the change.
    # Reported by:              If someone else reported the issue.
    # Reviewed by:              If someone else reviewed your modification.
    # Approved by:              If you needed approval for this commit.
    # Obtained from:            If the change is from a third party.
    # MFC after:                N [day[s]|week[s]|month[s]].  Request a reminder email.
    # MFH:                      Ports tree branch name.  Request approval for merge.
    # Relnotes:                 Set to 'yes' for mention in release notes.
    # Security:                 Vulnerability reference (one per line) or description.
    # Sponsored by:             If the change was sponsored by an organization.
    # Differential Revision:    https://reviews.freebsd.org/D### (*full* phabric URL needed).
    #
    # See 'Commit Log Messages' in the Committer's Guide for details.
    #
    # -------------------------------------------------------------------------
    # To use this template, run this command from the top of the ports tree.
    #
    #	git config commit.template .gitmessage
    # -------------------------------------------------------------------------

--- dns/dsc/files/patch-cron_upload-prep.pl
+++ dns/dsc/files/patch-cron_upload-prep.pl
@@ -1,11 +1,11 @@
---- cron/upload-prep.pl.orig	2017-08-21 08:14:08 UTC
+--- cron/upload-prep.pl.orig	2019-02-13 09:12:28 UTC
 +++ cron/upload-prep.pl
 @@ -1,4 +1,4 @@
 -#!/usr/bin/perl
 +#!/usr/local/bin/perl
  #
- # Copyright (c) 2016-2017, OARC, Inc.
- # Copyright (c) 2007, The Measurement Factory, Inc.
+ # Copyright (c) 2008-2019, OARC, Inc.
+ # Copyright (c) 2007-2008, Internet Systems Consortium, Inc.
 @@ -54,7 +54,7 @@ exit 0 if Proc::PID::File->running(dir =
  sleep 3;
  
