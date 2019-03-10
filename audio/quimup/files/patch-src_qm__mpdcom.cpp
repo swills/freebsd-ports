9230420fc0b021294dc65f8d38e3e5af2a75d386
Steve Wills <steve@mouf.net>
2019-03-09 21:04:28 -0500

    audio/quimup:
    PR:		236437
    Submitted by:
    Reported by:
    Reviewed by:
    Approved by:
    Obtained from:
    MFC after:
    MFH:
    Relnotes:
    Security:
    Sponsored by:
    Differential Revision:
    #
    # Description of fields to fill in above:                     76 columns --|
    # PR:                       If and which Problem Report is related.
    # Submitted by:             If someone else sent in the change.
    # Reported by:              If someone else reported the issue.
    # Reviewed by:              If someone else reviewed your modification.
    # Approved by:              If you needed approval for this commit.
    # Obtained from:            If the change is from a third party.
    # MFC after:                N [day[s]|week[s]|month[s]].  Request a reminder email.
    # MFH:                      Ports tree branch name.  Request approval for merge.
    # Relnotes:                 Set to 'yes' for mention in release notes.
    # Security:                 Vulnerability reference (one per line) or description.
    # Sponsored by:             If the change was sponsored by an organization.
    # Differential Revision:    https://reviews.freebsd.org/D### (*full* phabric URL needed).
    #
    # See 'Commit Log Messages' in the Committer's Guide for details.
    #
    # -------------------------------------------------------------------------
    # To use this template, run this command from the top of the ports tree.
    #
    #	git config commit.template .gitmessage
    # -------------------------------------------------------------------------

--- audio/quimup/files/patch-src_qm__mpdcom.cpp
+++ audio/quimup/files/patch-src_qm__mpdcom.cpp
@@ -1,6 +1,39 @@
---- src/qm_mpdcom.cpp.orig	2016-02-28 09:52:46 UTC
-+++ src/qm_mpdcom.cpp
-@@ -2500,17 +2500,10 @@ bool qm_mpdCom::is_mpd_running()
+--- src/qm_mpdcom.cpp.orig	2018-12-11 16:49:38.000000000 -0500
++++ src/qm_mpdcom.cpp	2019-03-09 20:14:42.938535000 -0500
+@@ -367,16 +367,16 @@
+     QFile file;
+     QString line = "";
+ 
+-    file.setFileName("/etc/default/mpd");
++    file.setFileName("/usr/local/etc/musicpd.conf");
+     if ( !file.exists() )
+     {
+-        printf ("MPD's config : Tried /etc/default/mpd but it does not exist\n");
++        printf ("MPD's config : Tried /usr/local/etc/musicpd.conf but it does not exist\n");
+         return line;
+     }
+ 
+     if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+     {
+-        printf ("MPD's config : Tried /etc/default/mpd but it is not readable\n");
++        printf ("MPD's config : Tried /usr/local/etc/musicpd.conf but it is not readable\n");
+         return line;
+     }
+ 
+@@ -393,10 +393,10 @@
+     file.close();
+ 
+     if (line.isEmpty())
+-        printf ("MPD's config : Tried /etc/default/mpd but MPDCONF is not set\n");
++        printf ("MPD's config : Tried /usr/local/etc/musicpd.conf but MPDCONF is not set\n");
+     else
+     {
+-        QString msg  = "MPD's config found in /etc/default/mpd : " + line + "\n";
++        QString msg  = "MPD's config found in /usr/local/etc/musicpd.conf : " + line + "\n";
+         printf ("%s", msg.toUtf8().constData());
+     }
+ 
+@@ -2710,17 +2710,10 @@
      struct stat sts;
      bool b_isrunning = false;
  
