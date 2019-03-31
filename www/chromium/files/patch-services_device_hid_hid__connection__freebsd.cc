swills/freebsd-ports

commit 3effdce602c7f04a8f534f409afdc51ac88bb2b3

www/chromium: Update to 71.0.3578.98
Changelog: https://chromium.googlesource.com/chromium/src/+log/68.0.3440.106..71.0.3578.98?pretty=fuller&n=10000

PR:		233990
Submitted by:	Matthias Wolf <freebsd@rheinwolf.de>
Reported by:	nikolaibitinit@gmail.com
Reviewed by:	rene, cpm
Approved by:	rene, cpm
MFH:		2019Q1
Security:	http://vuxml.freebsd.org/freebsd/546d4dd4-10ea-11e9-b407-080027ef1a23.html
Security:	http://vuxml.freebsd.org/freebsd/720590df-10eb-11e9-b407-080027ef1a23.html


git-svn-id: svn+ssh://svn.freebsd.org/ports/head@489612 35697150-7ecd-e111-bb59-0022644237b5

www/chromium/Makefile
@@ -2,8 +2,7 @@
 # $FreeBSD$
 
 PORTNAME=	chromium
-PORTVERSION=	68.0.3440.106
-PORTREVISION=	7
+PORTVERSION=	71.0.3578.98
 CATEGORIES?=	www
 MASTER_SITES=	https://commondatastorage.googleapis.com/chromium-browser-official/ \
 		LOCAL/cpm/chromium/:fonts
@@ -25,6 +24,7 @@ BUILD_DEPENDS+=	gperf:devel/gperf \
 		ffmpeg>=3.2.2,1:multimedia/ffmpeg \
 		flock:sysutils/flock \
 		node:www/node \
+		${LOCALBASE}/bin/ar:devel/binutils \
 		${LOCALBASE}/include/linux/videodev2.h:multimedia/v4l_compat \
 		${LOCALBASE}/share/usbids/usb.ids:misc/usbids \
 		${PYTHON_PKGNAMEPREFIX}html5lib>0:www/py-html5lib@${PY_FLAVOR}
@@ -32,6 +32,7 @@ BUILD_DEPENDS+=	gperf:devel/gperf \
 
 .if !defined(GN_ONLY)
 LIB_DEPENDS=	libatk-bridge-2.0.so:accessibility/at-spi2-atk \
+		libatspi.so:accessibility/at-spi2-core \
 		libspeechd.so:accessibility/speech-dispatcher \
 		libsnappy.so:archivers/snappy \
 		libFLAC.so:audio/flac \
@@ -72,11 +73,12 @@ ONLY_FOR_ARCHS=			aarch64 amd64 i386
 USES=		compiler:c++14-lang localbase:ldflags ninja pkgconfig \
 		python:2.7,build shebangfix tar:xz
 .else
-USES=		bison cpe desktop-file-utils gnome jpeg localbase:ldflags \
+USES=		bison cpe desktop-file-utils dos2unix gl gnome jpeg localbase:ldflags \
 		ninja perl5 pkgconfig python:2.7,build shebangfix tar:xz
 .endif
 MAKE_ARGS=	-C out/${BUILDTYPE}
 BINARY_ALIAS=	python=${PYTHON_CMD}
+DOS2UNIX_FILES=	third_party/skia/third_party/vulkanmemoryallocator/include/vk_mem_alloc.h
 
 .if !defined(GN_ONLY)
 CPE_VENDOR=	google
@@ -107,6 +109,7 @@ GN_ARGS+=	clang_use_chrome_plugins=false \
 		enable_remoting=false \
 		fieldtrial_testing_like_official_build=true \
 		is_clang=true \
+		jumbo_file_merge_limit=8 \
 		toolkit_views=true \
 		treat_warnings_as_errors=false \
 		use_allocator="none" \
@@ -115,7 +118,7 @@ GN_ARGS+=	clang_use_chrome_plugins=false \
 		use_bundled_fontconfig=false \
 		use_custom_libcxx=false \
 		use_gnome_keyring=false \
-		use_gtk3=true \
+		use_jumbo_build=true \
 		use_lld=true \
 		use_sysroot=false \
 		use_system_freetype=true \
@@ -125,7 +128,7 @@ GN_ARGS+=	clang_use_chrome_plugins=false \
 		extra_ldflags="${LDFLAGS}"
 # TODO: investigate building with these options:
 # use_system_minigbm
-GN_BOOTSTRAP_FLAGS=	--no-clean --no-rebuild
+GN_BOOTSTRAP_FLAGS=	--no-clean --no-rebuild --skip-generate-buildfiles
 
 # FreeBSD Chromium Api Key
 # Set up Google API keys, see http://www.chromium.org/developers/how-tos/api-keys .
@@ -206,13 +209,13 @@ TEST_DISTFILES=		${PORTNAME}-${DISTVERSION}-testdata${EXTRACT_SUFX} \
 
 # swiftshader/lib/{libEGL.so,libGLESv2.so} is x86 only
 .if ${ARCH} == aarch64
-PLIST_SUB+=    NOT_AARCH64="@comment "
+PLIST_SUB+=	NOT_AARCH64="@comment "
 .else
 PLIST_SUB+=	NOT_AARCH64=""
 .endif
 
-BUILD_DEPENDS+=	${LOCALBASE}/bin/ar:devel/binutils
-EXTRA_PATCHES+=	${FILESDIR}/extra-patch-llvm-base
+# Allow relocations against read-only segments (override lld default)
+LDFLAGS_i386=	-Wl,-znotext
 
 # TODO: -isystem, would be just as ugly as this approach, but more reliably
 # build would fail without C_INCLUDE_PATH/CPLUS_INCLUDE_PATH env var set.
@@ -266,6 +269,7 @@ do-configure:
 .endif
 
 do-test-TEST-on:
+	cd ${WRKSRC}/out/${BUILDTYPE}/gn_build && ./gn_unittests
 .for t in ${TEST_TARGETS}
 	cd ${WRKSRC}/out/${BUILDTYPE} && ${SETENV} LC_ALL=en_US.UTF-8 \
 		./${t} --gtest_filter=-${EXCLUDE_${t}:ts:} || ${TRUE}
@@ -276,7 +280,6 @@ do-install:
 	@${MKDIR} ${STAGEDIR}${DATADIR}
 	${INSTALL_MAN} ${WRKSRC}/chrome/app/resources/manpage.1.in \
 		${STAGEDIR}${MANPREFIX}/man/man1/chrome.1
-	${INSTALL_DATA} ${WRKSRC}/out/${BUILDTYPE}/*.service ${STAGEDIR}${DATADIR}
 .for s in 22 24 48 64 128 256
 	@${MKDIR} ${STAGEDIR}${PREFIX}/share/icons/hicolor/${s}x${s}/apps
 	${INSTALL_DATA} ${WRKSRC}/chrome/app/theme/chromium/product_logo_${s}.png \

www/chromium/files/patch-services_device_hid_hid__connection__freebsd.cc
@@ -1,6 +1,6 @@
---- services/device/hid/hid_connection_freebsd.cc.orig	2018-07-20 13:47:11.556393000 +0200
-+++ services/device/hid/hid_connection_freebsd.cc	2018-07-20 15:22:57.210103000 +0200
-@@ -0,0 +1,277 @@
+--- services/device/hid/hid_connection_freebsd.cc.orig	2018-12-27 21:14:54.188404000 +0100
++++ services/device/hid/hid_connection_freebsd.cc	2018-12-29 17:34:19.982594000 +0100
+@@ -0,0 +1,191 @@
 +// Copyright (c) 2014 The Chromium Authors. All rights reserved.
 +// Use of this source code is governed by a BSD-style license that can be
 +// found in the LICENSE file.
@@ -17,7 +17,7 @@
 +#include "base/posix/eintr_wrapper.h"
 +#include "base/single_thread_task_runner.h"
 +#include "base/strings/stringprintf.h"
-+#include "base/task_scheduler/post_task.h"
++#include "base/task/post_task.h"
 +#include "base/threading/thread_restrictions.h"
 +#include "base/threading/thread_task_runner_handle.h"
 +#include "components/device_event_log/device_event_log.h"
@@ -46,10 +46,6 @@
 +  void Start() {
 +    DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 +    base::AssertBlockingAllowed();
-+
-+    file_watcher_ = base::FileDescriptorWatcher::WatchReadable(
-+        fd_.get(), base::Bind(&BlockingTaskHelper::OnFileCanReadWithoutBlocking,
-+                              base::Unretained(this)));
 +  }
 +
 +  void Write(scoped_refptr<base::RefCountedBytes> buffer,
@@ -126,49 +122,12 @@
 +  }
 +
 + private:
-+  void OnFileCanReadWithoutBlocking() {
-+    DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-+
-+    scoped_refptr<base::RefCountedBytes> buffer(new base::RefCountedBytes(report_buffer_size_));
-+    unsigned char* data = buffer->front();
-+    size_t length = report_buffer_size_;
-+    if (!has_report_id_) {
-+      // FreeBSD will not prefix the buffer with a report ID if report IDs are not
-+      // used by the device. Prefix the buffer with 0.
-+      *data++ = 0;
-+      length--;
-+    }
-+
-+    ssize_t bytes_read = HANDLE_EINTR(read(fd_.get(), data, length));
-+    if (bytes_read < 0) {
-+      if (errno != EAGAIN) {
-+        HID_PLOG(EVENT) << "Read failed";
-+        // This assumes that the error is unrecoverable and disables reading
-+        // from the device until it has been re-opened.
-+        // TODO(reillyg): Investigate starting and stopping the file descriptor
-+        // watcher in response to pending read requests so that per-request
-+        // errors can be returned to the client.
-+        file_watcher_.reset();
-+      }
-+      return;
-+    }
-+    if (!has_report_id_) {
-+      // Behave as if the byte prefixed above as the the report ID was read.
-+      bytes_read++;
-+    }
-+
-+    origin_task_runner_->PostTask(
-+        FROM_HERE, base::BindOnce(&HidConnectionFreeBSD::ProcessInputReport,
-+                              connection_, buffer, bytes_read));
-+  }
-+
 +  SEQUENCE_CHECKER(sequence_checker_);
 +  base::ScopedFD fd_;
 +  size_t report_buffer_size_;
 +  bool has_report_id_;
 +  base::WeakPtr<HidConnectionFreeBSD> connection_;
 +  const scoped_refptr<base::SequencedTaskRunner> origin_task_runner_;
-+  std::unique_ptr<base::FileDescriptorWatcher::Controller> file_watcher_;
 +
 +  DISALLOW_COPY_AND_ASSIGN(BlockingTaskHelper);
 +};
@@ -195,18 +154,6 @@
 +  // and 2) any tasks posted to this task runner that refer to this file will
 +  // complete before it is closed.
 +  blocking_task_runner_->DeleteSoon(FROM_HERE, helper_.release());
-+
-+  while (!pending_reads_.empty()) {
-+    std::move(pending_reads_.front().callback).Run(false, NULL, 0);
-+    pending_reads_.pop();
-+  }
-+}
-+
-+void HidConnectionFreeBSD::PlatformRead(ReadCallback callback) {
-+  PendingHidRead pending_read;
-+  pending_read.callback = std::move(callback);
-+  pending_reads_.push(std::move(pending_read));
-+  ProcessReadQueue();
 +}
 +
 +void HidConnectionFreeBSD::PlatformWrite(scoped_refptr<base::RefCountedBytes> buffer,
@@ -242,39 +189,6 @@
 +      FROM_HERE,
 +      base::BindOnce(&BlockingTaskHelper::SendFeatureReport,
 +                 base::Unretained(helper_.get()), buffer, std::move(callback)));
-+}
-+
-+void HidConnectionFreeBSD::ProcessInputReport(
-+    scoped_refptr<base::RefCountedBytes> buffer,
-+    size_t size) {
-+  DCHECK(thread_checker().CalledOnValidThread());
-+  DCHECK_GE(size, 1u);
-+
-+  uint8_t report_id = buffer->data()[0];
-+  if (IsReportIdProtected(report_id))
-+    return;
-+
-+  PendingHidReport report;
-+  report.buffer = buffer;
-+  report.size = size;
-+  pending_reports_.push(report);
-+  ProcessReadQueue();
-+}
-+
-+void HidConnectionFreeBSD::ProcessReadQueue() {
-+  DCHECK(thread_checker().CalledOnValidThread());
-+
-+  // Hold a reference to |this| to prevent a callback from freeing this object
-+  // during the loop.
-+  scoped_refptr<HidConnectionFreeBSD> self(this);
-+  while (pending_reads_.size() && pending_reports_.size()) {
-+    PendingHidRead read = std::move(pending_reads_.front());
-+    PendingHidReport report = std::move(pending_reports_.front());
-+
-+    pending_reads_.pop();
-+    pending_reports_.pop();
-+    std::move(read.callback).Run(true, std::move(report.buffer), report.size);
-+  }
 +}
 +
 +}  // namespace device

www/chromium/files/patch-services_device_hid_hid__connection__freebsd.h
@@ -1,6 +1,6 @@
---- services/device/hid/hid_connection_freebsd.h.orig	2018-03-26 19:51:55.337385000 -0700
-+++ services/device/hid/hid_connection_freebsd.h	2018-03-26 22:46:52.914490000 -0700
-@@ -0,0 +1,77 @@
+--- services/device/hid/hid_connection_freebsd.h.orig	2018-12-27 21:14:54.190483000 +0100
++++ services/device/hid/hid_connection_freebsd.h	2018-12-28 15:27:41.475526000 +0100
+@@ -0,0 +1,70 @@
 +// Copyright (c) 2014 The Chromium Authors. All rights reserved.
 +// Use of this source code is governed by a BSD-style license that can be
 +// found in the LICENSE file.
@@ -10,8 +10,6 @@
 +
 +#include <stddef.h>
 +#include <stdint.h>
-+
-+#include <queue>
 +
 +#include "base/files/scoped_file.h"
 +#include "base/macros.h"
@@ -46,7 +44,6 @@
 +
 +  // HidConnection implementation.
 +  void PlatformClose() override;
-+  void PlatformRead(ReadCallback callback) override;
 +  void PlatformWrite(scoped_refptr<base::RefCountedBytes> buffer,
 +                     WriteCallback callback) override;
 +  void PlatformGetFeatureReport(uint8_t report_id,
@@ -55,7 +52,6 @@
 +                                 WriteCallback callback) override;
 +  void ProcessInputReport(scoped_refptr<base::RefCountedBytes> buffer,
 +                          size_t size);
-+  void ProcessReadQueue();
 +
 +  // |helper_| lives on the sequence to which |blocking_task_runner_| posts
 +  // tasks so all calls must be posted there including this object's
@@ -63,9 +59,6 @@
 +  std::unique_ptr<BlockingTaskHelper> helper_;
 +
 +  const scoped_refptr<base::SequencedTaskRunner> blocking_task_runner_;
-+
-+  std::queue<PendingHidReport> pending_reports_;
-+  std::queue<PendingHidRead> pending_reads_;
 +  const scoped_refptr<base::SequencedTaskRunner> task_runner_;
 +
 +  SEQUENCE_CHECKER(sequence_checker_);

www/chromium/files/patch-services_device_hid_hid__service__freebsd.cc
@@ -1,5 +1,5 @@
---- services/device/hid/hid_service_freebsd.cc.orig	2018-07-20 13:47:11.569682000 +0200
-+++ services/device/hid/hid_service_freebsd.cc	2018-07-20 15:20:19.980971000 +0200
+--- services/device/hid/hid_service_freebsd.cc.orig	2018-12-27 21:14:54.193271000 +0100
++++ services/device/hid/hid_service_freebsd.cc	2018-12-28 15:20:01.597855000 +0100
 @@ -0,0 +1,371 @@
 +// Copyright 2014 The Chromium Authors. All rights reserved.
 +// Use of this source code is governed by a BSD-style license that can be
@@ -29,7 +29,7 @@
 +#include "base/strings/sys_string_conversions.h"
 +#include "base/strings/string_util.h"
 +#include "base/strings/string_split.h"
-+#include "base/task_scheduler/post_task.h"
++#include "base/task/post_task.h"
 +#include "base/threading/thread_restrictions.h"
 +#include "base/threading/thread_task_runner_handle.h"
 +#include "components/device_event_log/device_event_log.h"

commit 23d135e6a732107bdbfdb47ead15c54da297cb7e

www/chromium: fix U2F support
U2F (ie Yubikeys) support was broken in r489612 when part of the required functionality was removed from services/device/hid/hid_connection_freebsd.cc

Submitted patch brings it back and also synchronizes blocking checks API with Linux version of the file.

PR:		236911
Submitted by:	gonzo

www/chromium/Makefile
@@ -3,7 +3,7 @@
 
 PORTNAME=	chromium
 PORTVERSION=	72.0.3626.121
-PORTREVISION=	2
+PORTREVISION=	3
 CATEGORIES?=	www
 MASTER_SITES=	https://commondatastorage.googleapis.com/chromium-browser-official/ \
 		LOCAL/cpm/chromium/:fonts

www/chromium/files/patch-services_device_hid_hid__connection__freebsd.cc
@@ -1,6 +1,6 @@
---- services/device/hid/hid_connection_freebsd.cc.orig	2019-02-01 16:07:39.214979000 +0100
-+++ services/device/hid/hid_connection_freebsd.cc	2019-02-02 01:21:12.648154000 +0100
-@@ -0,0 +1,191 @@
+--- services/device/hid/hid_connection_freebsd.cc.orig	2019-03-30 17:42:59.718158000 -0700
++++ services/device/hid/hid_connection_freebsd.cc	2019-03-30 21:54:38.653951000 -0700
+@@ -0,0 +1,240 @@
 +// Copyright (c) 2014 The Chromium Authors. All rights reserved.
 +// Use of this source code is governed by a BSD-style license that can be
 +// found in the LICENSE file.
@@ -18,6 +18,7 @@
 +#include "base/single_thread_task_runner.h"
 +#include "base/strings/stringprintf.h"
 +#include "base/task/post_task.h"
++#include "base/threading/scoped_blocking_call.h"
 +#include "base/threading/thread_restrictions.h"
 +#include "base/threading/thread_task_runner_handle.h"
 +#include "components/device_event_log/device_event_log.h"
@@ -46,11 +47,18 @@
 +  void Start() {
 +    DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 +    base::internal::AssertBlockingAllowed();
++
++    file_watcher_ = base::FileDescriptorWatcher::WatchReadable(
++        fd_.get(), base::Bind(&BlockingTaskHelper::OnFileCanReadWithoutBlocking,
++                              base::Unretained(this)));
 +  }
 +
 +  void Write(scoped_refptr<base::RefCountedBytes> buffer,
 +             WriteCallback callback) {
 +    DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
++    base::ScopedBlockingCall scoped_blocking_call(
++        base::BlockingType::MAY_BLOCK);
++
 +    auto data = buffer->front();
 +    size_t size = buffer->size();
 +    // if report id is 0, it shouldn't be included
@@ -74,6 +82,8 @@
 +                        scoped_refptr<base::RefCountedBytes> buffer,
 +                        ReadCallback callback) {
 +    DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
++    base::ScopedBlockingCall scoped_blocking_call(
++        base::BlockingType::MAY_BLOCK);
 +    struct usb_gen_descriptor ugd;
 +    ugd.ugd_report_type = UHID_FEATURE_REPORT;
 +    ugd.ugd_data = buffer->front();
@@ -122,12 +132,49 @@
 +  }
 +
 + private:
++  void OnFileCanReadWithoutBlocking() {
++    DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
++
++    scoped_refptr<base::RefCountedBytes> buffer(new base::RefCountedBytes(report_buffer_size_));
++    unsigned char* data = buffer->front();
++    size_t length = report_buffer_size_;
++    if (!has_report_id_) {
++      // FreeBSD will not prefix the buffer with a report ID if report IDs are not
++      // used by the device. Prefix the buffer with 0.
++      *data++ = 0;
++      length--;
++    }
++
++    ssize_t bytes_read = HANDLE_EINTR(read(fd_.get(), data, length));
++    if (bytes_read < 0) {
++      if (errno != EAGAIN) {
++        HID_PLOG(EVENT) << "Read failed";
++        // This assumes that the error is unrecoverable and disables reading
++        // from the device until it has been re-opened.
++        // TODO(reillyg): Investigate starting and stopping the file descriptor
++        // watcher in response to pending read requests so that per-request
++        // errors can be returned to the client.
++        file_watcher_.reset();
++      }
++      return;
++    }
++    if (!has_report_id_) {
++      // Behave as if the byte prefixed above as the the report ID was read.
++      bytes_read++;
++    }
++
++    origin_task_runner_->PostTask(
++        FROM_HERE, base::BindOnce(&HidConnectionFreeBSD::ProcessInputReport,
++                              connection_, buffer, bytes_read));
++  }
++
 +  SEQUENCE_CHECKER(sequence_checker_);
 +  base::ScopedFD fd_;
 +  size_t report_buffer_size_;
 +  bool has_report_id_;
 +  base::WeakPtr<HidConnectionFreeBSD> connection_;
 +  const scoped_refptr<base::SequencedTaskRunner> origin_task_runner_;
++  std::unique_ptr<base::FileDescriptorWatcher::Controller> file_watcher_;
 +
 +  DISALLOW_COPY_AND_ASSIGN(BlockingTaskHelper);
 +};
@@ -185,6 +232,8 @@
 +void HidConnectionFreeBSD::PlatformSendFeatureReport(
 +    scoped_refptr<base::RefCountedBytes> buffer,
 +    WriteCallback callback) {
++  base::ScopedBlockingCall scoped_blocking_call(
++      base::BlockingType::MAY_BLOCK);
 +  blocking_task_runner_->PostTask(
 +      FROM_HERE,
 +      base::BindOnce(&BlockingTaskHelper::SendFeatureReport,

www/chromium/files/patch-services_device_hid_hid__connection__freebsd.h
@@ -1,6 +1,6 @@
---- services/device/hid/hid_connection_freebsd.h.orig	2018-12-27 21:14:54.190483000 +0100
-+++ services/device/hid/hid_connection_freebsd.h	2018-12-28 15:27:41.475526000 +0100
-@@ -0,0 +1,70 @@
+--- services/device/hid/hid_connection_freebsd.h.orig	2019-03-30 17:42:59.719257000 -0700
++++ services/device/hid/hid_connection_freebsd.h	2019-03-30 21:53:54.126040000 -0700
+@@ -0,0 +1,68 @@
 +// Copyright (c) 2014 The Chromium Authors. All rights reserved.
 +// Use of this source code is governed by a BSD-style license that can be
 +// found in the LICENSE file.
@@ -50,8 +50,6 @@
 +                                ReadCallback callback) override;
 +  void PlatformSendFeatureReport(scoped_refptr<base::RefCountedBytes> buffer,
 +                                 WriteCallback callback) override;
-+  void ProcessInputReport(scoped_refptr<base::RefCountedBytes> buffer,
-+                          size_t size);
 +
 +  // |helper_| lives on the sequence to which |blocking_task_runner_| posts
 +  // tasks so all calls must be posted there including this object's

www/chromium/files/patch-services_device_hid_hid__service__freebsd.cc
@@ -1,6 +1,6 @@
---- services/device/hid/hid_service_freebsd.cc.orig	2019-02-01 16:07:39.219688000 +0100
-+++ services/device/hid/hid_service_freebsd.cc	2019-02-02 00:41:58.747111000 +0100
-@@ -0,0 +1,371 @@
+--- services/device/hid/hid_service_freebsd.cc.orig	2019-03-30 17:42:59.721025000 -0700
++++ services/device/hid/hid_service_freebsd.cc	2019-03-30 22:02:19.316167000 -0700
+@@ -0,0 +1,373 @@
 +// Copyright 2014 The Chromium Authors. All rights reserved.
 +// Use of this source code is governed by a BSD-style license that can be
 +// found in the LICENSE file.
@@ -30,7 +30,7 @@
 +#include "base/strings/string_util.h"
 +#include "base/strings/string_split.h"
 +#include "base/task/post_task.h"
-+#include "base/threading/thread_restrictions.h"
++#include "base/threading/scoped_blocking_call.h"
 +#include "base/threading/thread_task_runner_handle.h"
 +#include "components/device_event_log/device_event_log.h"
 +#include "services/device/hid/hid_connection_freebsd.h"
@@ -71,7 +71,6 @@
 +  }
 +
 +  void Start() {
-+    base::internal::AssertBlockingAllowed();
 +    DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 +
 +    const base::FilePath kDevRoot("/dev");
@@ -111,6 +110,8 @@
 +  }
 +
 +  void OnDeviceAdded(std::string device_id) {
++    base::ScopedBlockingCall scoped_blocking_call(
++        base::BlockingType::MAY_BLOCK);
 +    std::string device_node = "/dev/" + device_id;
 +    uint16_t vendor_id = 0xffff;
 +    uint16_t product_id = 0xffff;
@@ -169,6 +170,8 @@
 +  }
 +
 +  void OnDeviceRemoved(std::string device_id) {
++    base::ScopedBlockingCall scoped_blocking_call(
++        base::BlockingType::MAY_BLOCK);
 +    task_runner_->PostTask(
 +        FROM_HERE, base::Bind(&HidServiceFreeBSD::RemoveDevice, service_,
 +                              device_id));
@@ -312,7 +315,7 @@
 +// static
 +void HidServiceFreeBSD::OpenOnBlockingThread(
 +    std::unique_ptr<ConnectParams> params) {
-+  base::internal::AssertBlockingAllowed();
++  base::ScopedBlockingCall scoped_blocking_call(base::BlockingType::MAY_BLOCK);
 +  scoped_refptr<base::SequencedTaskRunner> task_runner = params->task_runner;
 +
 +  base::FilePath device_path(params->device_info->device_node());
@@ -333,7 +336,7 @@
 +
 +void HidServiceFreeBSD::Connect(const std::string& device_guid,
 +                            const ConnectCallback& callback) {
-+  DCHECK(thread_checker_.CalledOnValidThread());
++  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 +
 +  const auto& map_entry = devices().find(device_guid);
 +  if (map_entry == devices().end()) {
@@ -355,7 +358,6 @@
 +
 +// static
 +void HidServiceFreeBSD::FinishOpen(std::unique_ptr<ConnectParams> params) {
-+  base::internal::AssertBlockingAllowed();
 +  scoped_refptr<base::SequencedTaskRunner> task_runner = params->task_runner;
 +
 +  task_runner->PostTask(