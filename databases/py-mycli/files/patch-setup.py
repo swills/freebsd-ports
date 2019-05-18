4ae9fa7f787bde8523017e3e2f0e40307b2f5483
Steve Wills <steve@mouf.net>
2019-05-17 20:03:40 -0400

    databases/py-mycli:
    PR:		237724
    Submitted by:
    Reported by:
    Reviewed by:
    Approved by:
    Obtained from:
    MFC after:
    MFH:
    Relnotes:
    Security:
    Sponsored by:
    Differential Revision:
    #
    # Description of fields to fill in above:                     76 columns --|
    # PR:                       If and which Problem Report is related.
    # Submitted by:             If someone else sent in the change.
    # Reported by:              If someone else reported the issue.
    # Reviewed by:              If someone else reviewed your modification.
    # Approved by:              If you needed approval for this commit.
    # Obtained from:            If the change is from a third party.
    # MFC after:                N [day[s]|week[s]|month[s]].  Request a reminder email.
    # MFH:                      Ports tree branch name.  Request approval for merge.
    # Relnotes:                 Set to 'yes' for mention in release notes.
    # Security:                 Vulnerability reference (one per line) or description.
    # Sponsored by:             If the change was sponsored by an organization.
    # Differential Revision:    https://reviews.freebsd.org/D### (*full* phabric URL needed).
    #
    # See 'Commit Log Messages' in the Committer's Guide for details.
    #
    # -------------------------------------------------------------------------
    # To use this template, run this command from the top of the ports tree.
    #
    #	git config commit.template .gitmessage
    # -------------------------------------------------------------------------

---
+++ databases/py-mycli/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2019-05-03 03:37:18.374374000 -0500
++++ setup.py	2019-05-03 03:37:32.155398000 -0500
+@@ -21,7 +21,7 @@
+     'Pygments >= 1.6',
+     'prompt_toolkit>=2.0.6',
+     'PyMySQL >= 0.9.2',
+-    'sqlparse>=0.2.2,<0.3.0',
++    'sqlparse>=0.2.2',
+     'configobj >= 5.0.5',
+     'cryptography >= 1.0.0',
+     'cli_helpers[styles] >= 1.0.1',
