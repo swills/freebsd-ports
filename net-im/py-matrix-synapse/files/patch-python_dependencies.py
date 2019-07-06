522b794a1a77db969bc8e9dd773c019c96bc1135
Steve Wills <steve@mouf.net>
2019-07-06 05:00:45 -0400

    net-im/py-matrix-synapse:
    PR:		239013
    Submitted by:
    Reported by:
    Reviewed by:
    Approved by:
    Obtained from:
    MFC after:
    MFH:
    Relnotes:
    Security:
    Sponsored by:
    Differential Revision:
    #
    # Description of fields to fill in above:                     76 columns --|
    # PR:                       If and which Problem Report is related.
    # Submitted by:             If someone else sent in the change.
    # Reported by:              If someone else reported the issue.
    # Reviewed by:              If someone else reviewed your modification.
    # Approved by:              If you needed approval for this commit.
    # Obtained from:            If the change is from a third party.
    # MFC after:                N [day[s]|week[s]|month[s]].  Request a reminder email.
    # MFH:                      Ports tree branch name.  Request approval for merge.
    # Relnotes:                 Set to 'yes' for mention in release notes.
    # Security:                 Vulnerability reference (one per line) or description.
    # Sponsored by:             If the change was sponsored by an organization.
    # Differential Revision:    https://reviews.freebsd.org/D### (*full* phabric URL needed).
    #
    # See 'Commit Log Messages' in the Committer's Guide for details.
    #
    # -------------------------------------------------------------------------
    # To use this template, run this command from the top of the ports tree.
    #
    #	git config commit.template .gitmessage
    # -------------------------------------------------------------------------

--- net-im/py-matrix-synapse/files/patch-python_dependencies.py
+++ net-im/py-matrix-synapse/files/patch-python_dependencies.py
@@ -1,11 +1,11 @@
---- synapse/python_dependencies.py.orig	2019-02-14 17:20:02.000000000 +0000
-+++ synapse/python_dependencies.py	2019-02-28 20:31:10.838648000 +0000
-@@ -62,7 +62,7 @@
+--- synapse/python_dependencies.py.orig	2019-07-06 10:05:12.487752833 +0200
++++ synapse/python_dependencies.py	2019-07-06 10:05:32.045702936 +0200
+@@ -67,7 +67,7 @@
      "six>=1.10",
      # prometheus_client 0.4.0 changed the format of counter metrics
      # (cf https://github.com/matrix-org/synapse/issues/4001)
 -    "prometheus_client>=0.0.18,<0.4.0",
 +    "prometheus_client>=0.0.18",
- 
      # we use attr.s(slots), which arrived in 16.0.0
      # Twisted 18.7.0 requires attrs>=17.4.0
+     "attrs>=17.4.0",
