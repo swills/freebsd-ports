c307829ddc975335566d2db44a05360015000388
Steve Wills <steve@mouf.net>
2019-12-12 19:01:49 -0500

    lang/pcc: pcc(1) cannot handle missing argument of option
    PR:		242276
    Submitted by:
    Reported by:
    Reviewed by:
    Approved by:
    Obtained from:
    MFC after:
    MFH:
    Relnotes:
    Security:
    Sponsored by:
    Differential Revision:
    #
    # Description of fields to fill in above:                     76 columns --|
    # PR:                       If and which Problem Report is related.
    # Submitted by:             If someone else sent in the change.
    # Reported by:              If someone else reported the issue.
    # Reviewed by:              If someone else reviewed your modification.
    # Approved by:              If you needed approval for this commit.
    # Obtained from:            If the change is from a third party.
    # MFC after:                N [day[s]|week[s]|month[s]].  Request a reminder email.
    # MFH:                      Ports tree branch name.  Request approval for merge.
    # Relnotes:                 Set to 'yes' for mention in release notes.
    # Security:                 Vulnerability reference (one per line) or description.
    # Sponsored by:             If the change was sponsored by an organization.
    # Differential Revision:    https://reviews.freebsd.org/D### (*full* phabric URL needed).
    #
    # See 'Commit Log Messages' in the Committer's Guide for details.
    #
    # -------------------------------------------------------------------------
    # To use this template, run this command from the top of the ports tree.
    #
    #	git config commit.template .gitmessage
    # -------------------------------------------------------------------------

--- lang/pcc/files/patch-cc_cc_cc.c
+++ lang/pcc/files/patch-cc_cc_cc.c
@@ -9,3 +9,21 @@
  #define DEFLIBDIRS	{ "/usr/lib/", 0 }
  #else
  #define DEFLIBDIRS	{ "/usr/lib/", "/usr/lib/" MULTIARCH_PATH "/", 0 }
+@@ -475,7 +475,7 @@
+ #endif
+ #endif
+ 
+-	while (--lac) {
++	while (--lac > 0) {
+ 		++lav;
+ 		argp = *lav;
+ 
+@@ -1464,7 +1464,7 @@
+ 		if (lav[0][l] != 0)
+ 			return &lav[0][l];
+ 	}
+-	if (lac == 0)
++	if (lac < 2)
+ 		errorx(8, "missing argument to '%s'", o);
+ 	lav++;
+ 	lac--;
