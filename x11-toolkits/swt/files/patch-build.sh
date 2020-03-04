e25b9f6f9a1400a709106704778e2e9aadf1c74d
Steve Wills <steve@mouf.net>
2020-03-03 23:32:44 -0500

    x11-toolkits/swt: update to 4.14
    PR:		244585
    Submitted by:
    Reported by:
    Reviewed by:
    Approved by:
    Obtained from:
    MFC after:
    MFH:
    Relnotes:
    Security:
    Sponsored by:
    Differential Revision:
    #
    # Description of fields to fill in above:                     76 columns --|
    # PR:                       If and which Problem Report is related.
    # Submitted by:             If someone else sent in the change.
    # Reported by:              If someone else reported the issue.
    # Reviewed by:              If someone else reviewed your modification.
    # Approved by:              If you needed approval for this commit.
    # Obtained from:            If the change is from a third party.
    # MFC after:                N [day[s]|week[s]|month[s]].  Request a reminder email.
    # MFH:                      Ports tree branch name.  Request approval for merge.
    # Relnotes:                 Set to 'yes' for mention in release notes.
    # Security:                 Vulnerability reference (one per line) or description.
    # Sponsored by:             If the change was sponsored by an organization.
    # Differential Revision:    https://reviews.freebsd.org/D### (*full* phabric URL needed).
    #
    # See 'Commit Log Messages' in the Committer's Guide for details.
    #
    # -------------------------------------------------------------------------
    # To use this template, run this command from the top of the ports tree.
    #
    #	git config commit.template .gitmessage
    # -------------------------------------------------------------------------

--- x11-toolkits/swt/files/patch-build.sh
+++ x11-toolkits/swt/files/patch-build.sh
@@ -1,84 +1,67 @@
---- build.sh.orig	2018-03-01 07:35:22.000000000 +0100
-+++ build.sh	2018-06-27 11:24:43.371971000 +0200
-@@ -115,9 +115,19 @@
- 			fi
- 		fi
+--- build.sh.orig	2019-12-09 22:30:04 UTC
++++ build.sh
+@@ -72,7 +72,7 @@ echo -e "${RED}*** ${@}${NC}"
+ 
+ cd `dirname $0`
+ 
+-MAKE_TYPE=make
++MAKE_TYPE=gmake
+ 
+ export CFLAGS='-O -Wall -fPIC'
+ 
+@@ -86,21 +86,14 @@ case $OS in
+ 		MAKEFILE=make_win32.mak
  		;;
-+	"DragonFly")
-+		SWT_OS=dragonfly
-+		MAKEFILE=make_dragonfly.mak
-+		if uname -p > /dev/null 2>&1; then
-+			MODEL=`uname -p`
-+		fi
+ 	*)
+-		SWT_OS=`uname -s | tr -s '[:upper:]' '[:lower:]'`
++		SWT_OS=`uname -s | tr '[:upper:]' '[:lower:]'`
+ 		MAKEFILE=make_linux.mak
+ 		;;
+ esac
+ 
+ # Determine which CPU type we are building for
+ if [ "${MODEL}" = "" ]; then
+-	if uname -i > /dev/null 2>&1; then
+-		MODEL=`uname -i`
+-		if [ ${MODEL} = 'unknown' ]; then
+-		  MODEL=`uname -m`
+-		fi
+-	else
+ 		MODEL=`uname -m`
+-	fi
+ fi
+ case $MODEL in
+ 	"x86_64")
+@@ -111,6 +104,11 @@ case $MODEL in
+ 		SWT_ARCH=x86
+ 		AWT_ARCH=i386
+ 		;;
++	"powerpc" | "powerpc64")
++		SWT_ARCH=ppc64
++		AWT_ARCH=ppc64
++		MODEL=`uname -p`
 +		;;
- 	"FreeBSD")
- 		SWT_OS=freebsd
- 		MAKEFILE=make_freebsd.mak
-+		if uname -p > /dev/null 2>&1; then
-+			MODEL=`uname -p`
-+		fi
- 		;;
- 	"Windows_NT")
- 		SWT_OS=win32
-@@ -469,10 +479,10 @@
+ 	*)
+ 		SWT_ARCH=$MODEL
+ 		AWT_ARCH=$MODEL
+@@ -194,7 +192,7 @@ esac
  
  
  # For 64-bit CPUs, we have a switch
--if [ ${MODEL} = 'x86_64' -o ${MODEL} = 'ppc64' -o ${MODEL} = 'ia64' -o ${MODEL} = 'sparcv9'  -o ${MODEL} = 's390x' -o ${MODEL} = 'ppc64le' -o ${MODEL} = 'aarch64' ]; then
-+if [ ${MODEL} = 'x86_64' -o ${MODEL} = 'ppc64' -o ${MODEL} = 'ia64' -o ${MODEL} = 'sparcv9'  -o ${MODEL} = 's390x' -o ${MODEL} = 'ppc64le' -o ${MODEL} = 'aarch64'  -o ${MODEL} = 'amd64' ]; then
+-if [ ${MODEL} = 'x86_64' -o ${MODEL} = 'ia64' -o ${MODEL} = 's390x' -o ${MODEL} = 'ppc64le' -o ${MODEL} = 'aarch64' ]; then
++if [ ${MODEL} = 'amd64' -o ${MODEL} = 'ia64' -o ${MODEL} = 's390x' -o ${MODEL} = 'ppc64le' -o ${MODEL} = 'aarch64' -o ${MODEL} = 'powerpc64' ]; then
  	SWT_PTR_CFLAGS=-DJNI64
  	if [ -d /lib64 ]; then
--		XLIB64=-L/usr/X11R6/lib64
-+		XLIB64=-L${LOCALBASE}/lib64
- 		export XLIB64
+ 		XLIB64=-L/usr/X11R6/lib64
+@@ -205,6 +203,11 @@ if [ ${MODEL} = 'x86_64' -o ${MODEL} = 'ia64' -o ${MOD
+ 		XLIB64="${XLIB64} -L/usr/lib64"
+ 		SWT_LFLAGS=-m64
+ 		export SWT_LFLAGS
++	fi
++	if [ ${SWT_OS} = "freebsd" ]
++	then
++		SWT_PTR_CFLAGS="${SWT_PTR_CFLAGS} -m64"
++		export SWT_LFLAGS=-m64
  	fi
- 	if [ ${MODEL} = 'ppc64' -o ${MODEL} = 'ppc64le' ]; then
-@@ -517,37 +527,24 @@
- 	export SWT_LFLAGS SWT_PTR_CFLAGS
+ 	export SWT_PTR_CFLAGS
  fi
- 
--
-+if [ x${MAKE_CAIRO} = "xmake_cairo" ]; then
- if [ x`pkg-config --exists cairo && echo YES` = "xYES" ]; then
- 	func_echo_plus "Cairo found, compiling SWT support for the cairo graphics library."
- 	MAKE_CAIRO=make_cairo
- else
- 	func_echo_error "Cairo not found: Advanced graphics support using cairo will not be compiled."
- fi
-+fi
- 
--if [ -z "${MOZILLA_INCLUDES}" -a -z "${MOZILLA_LIBS}" -a ${SWT_OS} != 'solaris' ]; then
--	if [ x`pkg-config --exists mozilla-xpcom && echo YES` = "xYES" ]; then
--		MOZILLA_INCLUDES=`pkg-config --cflags mozilla-xpcom`
--		MOZILLA_LIBS=`pkg-config --libs mozilla-xpcom`
--		export MOZILLA_INCLUDES
--		export MOZILLA_LIBS
--		MAKE_MOZILLA=make_mozilla
--	elif [ x`pkg-config --exists firefox-xpcom && echo YES` = "xYES" ]; then
--		MOZILLA_INCLUDES=`pkg-config --cflags firefox-xpcom`
--		MOZILLA_LIBS=`pkg-config --libs firefox-xpcom`
--		export MOZILLA_INCLUDES
--		export MOZILLA_LIBS
--		MAKE_MOZILLA=make_mozilla
--	elif [ x`pkg-config --exists libxul && echo YES` = "xYES" ]; then
--		XULRUNNER_INCLUDES=`pkg-config --cflags libxul`
--		XULRUNNER_LIBS=`pkg-config --libs libxul`
--		export XULRUNNER_INCLUDES
--		export XULRUNNER_LIBS
--		MAKE_MOZILLA=make_xulrunner
--	else
--		func_echo_error "None of the following libraries were found:  Mozilla/XPCOM, Firefox/XPCOM, or XULRunner/XPCOM:"
--		func_echo_error "   >> Mozilla embedding support will not be compiled."
--	fi
-+if [ x${MAKE_MOZILLA} = "xmake_xulrunner" ]; then
-+	echo "Using libxul for gecko support"
-+	XULRUNNER_INCLUDES=`pkg-config --cflags libxul libxul-embedding`
-+	XULRUNNER_LIBS=`pkg-config --libs libxul libxul-embedding`
-+	export XULRUNNER_INCLUDES
-+	export XULRUNNER_LIBS
-+else
-+	echo "None of the following libraries were found:  XULRunner/XPCOM"
-+	echo "    *** Mozilla embedding support will not be compiled."
- fi
- 
- # Find AWT if available
