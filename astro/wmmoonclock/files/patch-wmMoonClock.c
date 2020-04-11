e5c806d6394749df4198731e3c291241cfb43180
Steve Wills <steve@mouf.net>
2020-04-11 14:15:33 -0400

    astro/wmmoonclock: update to 1.29
    PR:		245539
    Submitted by:
    Reported by:
    Reviewed by:
    Approved by:
    Obtained from:
    MFC after:
    MFH:
    Relnotes:
    Security:
    Sponsored by:
    Differential Revision:
    #
    # Description of fields to fill in above:                     76 columns --|
    # PR:                       If and which Problem Report is related.
    # Submitted by:             If someone else sent in the change.
    # Reported by:              If someone else reported the issue.
    # Reviewed by:              If someone else reviewed your modification.
    # Approved by:              If you needed approval for this commit.
    # Obtained from:            If the change is from a third party.
    # MFC after:                N [day[s]|week[s]|month[s]].  Request a reminder email.
    # MFH:                      Ports tree branch name.  Request approval for merge.
    # Relnotes:                 Set to 'yes' for mention in release notes.
    # Security:                 Vulnerability reference (one per line) or description.
    # Sponsored by:             If the change was sponsored by an organization.
    # Differential Revision:    https://reviews.freebsd.org/D### (*full* phabric URL needed).
    #
    # See 'Commit Log Messages' in the Committer's Guide for details.
    #
    # -------------------------------------------------------------------------
    # To use this template, run this command from the top of the ports tree.
    #
    #	git config commit.template .gitmessage
    # -------------------------------------------------------------------------

--- astro/wmmoonclock/files/patch-wmMoonClock.c
+++ astro/wmmoonclock/files/patch-wmMoonClock.c
@@ -1,12 +1,12 @@
---- wmMoonClock.c.orig	1999-06-07 14:27:18 UTC
-+++ wmMoonClock.c
-@@ -162,7 +162,8 @@ int main(int argc, char *argv[]) {
-     int			i, n, k, j, ImageNumber;
-     int 		Year, Month, DayOfWeek, DayOfMonth;
-     int			Hours, Mins, Secs, OldSecs, digit, xoff, xsize;
+--- src/wmMoonClock.c.orig	2017-08-15 00:31:05.000000000 +0200
++++ src/wmMoonClock.c	2020-04-11 17:15:45.372609000 +0200
+@@ -157,7 +157,8 @@
+     struct tm		*GMTTime, *LocalTime;
+     XEvent		event;
+     int			i, n, j, ImageNumber, Year, Month, DayOfMonth, digit;
 -    long		CurrentLocalTime, CurrentGMTTime, date;
 +    time_t		CurrentLocalTime, CurrentGMTTime;
 +    long		date;
      double		UT, val, RA, DEC, UTRise, UTSet, LocalHour, hour24();
      int			D, H, M, S, sgn, A, B, q;
-     char		str[10];
+     CTrans           	c;
