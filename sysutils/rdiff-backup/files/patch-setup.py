d1b80e9bd6dbb33576a5762c78d5dd67bb289f8e
Steve Wills <steve@mouf.net>
2020-05-10 11:01:57 -0400

    sysutils/rdiff-backup: update to 2.0.0
    PR:		246250
    Submitted by:
    Reported by:
    Reviewed by:
    Approved by:
    Obtained from:
    MFC after:
    MFH:
    Relnotes:
    Security:
    Sponsored by:
    Differential Revision:
    #
    # Description of fields to fill in above:                     76 columns --|
    # PR:                       If and which Problem Report is related.
    # Submitted by:             If someone else sent in the change.
    # Reported by:              If someone else reported the issue.
    # Reviewed by:              If someone else reviewed your modification.
    # Approved by:              If you needed approval for this commit.
    # Obtained from:            If the change is from a third party.
    # MFC after:                N [day[s]|week[s]|month[s]].  Request a reminder email.
    # MFH:                      Ports tree branch name.  Request approval for merge.
    # Relnotes:                 Set to 'yes' for mention in release notes.
    # Security:                 Vulnerability reference (one per line) or description.
    # Sponsored by:             If the change was sponsored by an organization.
    # Differential Revision:    https://reviews.freebsd.org/D### (*full* phabric URL needed).
    #
    # See 'Commit Log Messages' in the Committer's Guide for details.
    #
    # -------------------------------------------------------------------------
    # To use this template, run this command from the top of the ports tree.
    #
    #	git config commit.template .gitmessage
    # -------------------------------------------------------------------------

--- sysutils/rdiff-backup/files/patch-setup.py
+++ sysutils/rdiff-backup/files/patch-setup.py
@@ -1,31 +1,17 @@
---- setup.py.orig	2008-08-06 21:24:28.756047734 +0200
-+++ setup.py	2008-08-06 21:24:18.122056264 +0200
-@@ -58,6 +58,10 @@
- 				'zipfile': None
- 			})
+--- setup.py.orig	2020-05-06 08:49:32 UTC
++++ setup.py
+@@ -136,7 +136,7 @@ class clean(distutils.command.clean.clean):
  
-+LOCALBASE = os.getenv("LOCALBASE", "/usr/local")
-+include_dirs = ["%s/include" % LOCALBASE]
-+library_dirs = ["%s/lib" % LOCALBASE]
-+
- setup(name="rdiff-backup",
- 	  version=version_string,
- 	  description="Local/remote mirroring+incremental backup",
-@@ -68,14 +72,8 @@
- 	  ext_modules = [Extension("rdiff_backup.C", ["cmodule.c"]),
- 					 Extension("rdiff_backup._librsync",
- 							   ["_librsyncmodule.c"],
--							   include_dirs=incdir_list,
--							   library_dirs=libdir_list,
-+							   include_dirs=include_dirs,
-+							   library_dirs=library_dirs,
- 							   libraries=libname,
- 							   extra_link_args=lflags_arg)],
--	  scripts = ['rdiff-backup', 'rdiff-backup-statistics'],
--	  data_files = [('share/man/man1', ['rdiff-backup.1',
--										'rdiff-backup-statistics.1']),
--					('share/doc/rdiff-backup-%s' % (version_string,),
--					 ['CHANGELOG', 'COPYING', 'README', 'FAQ.html'])],
--					**extra_options)
--
-+	  scripts = ['rdiff-backup', 'rdiff-backup-statistics'])
+ setup(
+     name="rdiff-backup",
+-    use_scm_version=True,
++    use_scm_version=False,
+     description="Backup and Restore utility, easy to use, efficient, locally and remotely usable",
+     long_description="""
+         rdiff-backup is a simple backup tool which can be used locally and remotely,
+@@ -222,5 +222,4 @@ setup(
+         'build_py': build_py,
+         'clean': clean,
+     },
+-    setup_requires=['setuptools_scm'],
+ )
