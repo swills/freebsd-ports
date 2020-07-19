e6d7da457fb79dae7255fab1e45a68b50264b90e
Steve Wills <steve@mouf.net>
2020-07-19 10:03:49 -0400

    editors/cooledit: Update to 4.0.0
    PR:		248107
    Submitted by:
    Reported by:
    Reviewed by:
    Approved by:
    Obtained from:
    MFC after:
    MFH:
    Relnotes:
    Security:
    Sponsored by:
    Differential Revision:
    #
    # Description of fields to fill in above:                     76 columns --|
    # PR:                       If and which Problem Report is related.
    # Submitted by:             If someone else sent in the change.
    # Reported by:              If someone else reported the issue.
    # Reviewed by:              If someone else reviewed your modification.
    # Approved by:              If you needed approval for this commit.
    # Obtained from:            If the change is from a third party.
    # MFC after:                N [day[s]|week[s]|month[s]].  Request a reminder email.
    # MFH:                      Ports tree branch name.  Request approval for merge.
    # Relnotes:                 Set to 'yes' for mention in release notes.
    # Security:                 Vulnerability reference (one per line) or description.
    # Sponsored by:             If the change was sponsored by an organization.
    # Differential Revision:    https://reviews.freebsd.org/D### (*full* phabric URL needed).
    #
    # See 'Commit Log Messages' in the Committer's Guide for details.
    #
    # -------------------------------------------------------------------------
    # To use this template, run this command from the top of the ports tree.
    #
    #	git config commit.template .gitmessage
    # -------------------------------------------------------------------------

--- editors/cooledit/files/patch-widget-widechar.c
+++ editors/cooledit/files/patch-widget-widechar.c
@@ -1,18 +1,18 @@
---- widget/widechar.c.orig	2017-08-04 19:12:31 UTC
-+++ widget/widechar.c
-@@ -227,6 +227,7 @@ static inline struct mb_rule apply_mb_ru
+--- widget/widechar.c.orig	2020-07-19 15:24:15.426672000 +0200
++++ widget/widechar.c	2020-07-19 15:26:49.202087000 +0200
+@@ -185,6 +185,7 @@
  static inline struct mb_rule apply_mb_rules_going_right (WEdit * edit, long byte_index, struct mb_rule mb_rule)
  {
  #ifdef HAVE_WCHAR_H
 +#ifdef HAVE_MBRTOWC
-     wchar_t wc;
+     C_wchar_t wc;
      unsigned char p[16];
      int n;
-@@ -253,6 +254,7 @@ static inline struct mb_rule apply_mb_ru
+@@ -216,6 +217,7 @@
+     }
      mb_rule.end = 0;
      mb_rule.ch = -1;
- #endif
 +#endif
+ #endif
      return mb_rule;
  }
- 
