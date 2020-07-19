4eee029f2b8358330b0d2f3c888064395a5c6bd8
Steve Wills <steve@mouf.net>
2020-07-18 20:01:28 -0400

    sysutils/minikube: fix build on non amd64 arches
    PR:		247755
    Submitted by:
    Reported by:
    Reviewed by:
    Approved by:
    Obtained from:
    MFC after:
    MFH:
    Relnotes:
    Security:
    Sponsored by:
    Differential Revision:
    #
    # Description of fields to fill in above:                     76 columns --|
    # PR:                       If and which Problem Report is related.
    # Submitted by:             If someone else sent in the change.
    # Reported by:              If someone else reported the issue.
    # Reviewed by:              If someone else reviewed your modification.
    # Approved by:              If you needed approval for this commit.
    # Obtained from:            If the change is from a third party.
    # MFC after:                N [day[s]|week[s]|month[s]].  Request a reminder email.
    # MFH:                      Ports tree branch name.  Request approval for merge.
    # Relnotes:                 Set to 'yes' for mention in release notes.
    # Security:                 Vulnerability reference (one per line) or description.
    # Sponsored by:             If the change was sponsored by an organization.
    # Differential Revision:    https://reviews.freebsd.org/D### (*full* phabric URL needed).
    #
    # See 'Commit Log Messages' in the Committer's Guide for details.
    #
    # -------------------------------------------------------------------------
    # To use this template, run this command from the top of the ports tree.
    #
    #	git config commit.template .gitmessage
    # -------------------------------------------------------------------------

---
+++ sysutils/minikube/files/patch-vendor_github.com_mitchellh_go-ps_process__freebsd.go
@@ -0,0 +1,8 @@
+--- vendor/github.com/mitchellh/go-ps/process_freebsd.go.orig	2020-07-04 11:42:25 UTC
++++ vendor/github.com/mitchellh/go-ps/process_freebsd.go
+@@ -1,4 +1,4 @@
+-// +build freebsd,amd64
++// +build freebsd
+ 
+ package ps
+ 
