8bdf67461771c1110fac94dd0199f92905918f5d
Steve Wills <steve@mouf.net>
2020-11-02 23:09:10 -0500

    www/apache24: Reword some sentences of README_modules.d
    PR:		250471
    Submitted by:
    Reported by:
    Reviewed by:
    Approved by:
    Obtained from:
    MFC after:
    MFH:
    Relnotes:
    Security:
    Sponsored by:
    Differential Revision:
    #
    # Description of fields to fill in above:                     76 columns --|
    # PR:                       If and which Problem Report is related.
    # Submitted by:             If someone else sent in the change.
    # Reported by:              If someone else reported the issue.
    # Reviewed by:              If someone else reviewed your modification.
    # Approved by:              If you needed approval for this commit.
    # Obtained from:            If the change is from a third party.
    # MFC after:                N [day[s]|week[s]|month[s]].  Request a reminder email.
    # MFH:                      Ports tree branch name.  Request approval for merge.
    # Relnotes:                 Set to 'yes' for mention in release notes.
    # Security:                 Vulnerability reference (one per line) or description.
    # Sponsored by:             If the change was sponsored by an organization.
    # Differential Revision:    https://reviews.freebsd.org/D### (*full* phabric URL needed).
    #
    # See 'Commit Log Messages' in the Committer's Guide for details.
    #
    # -------------------------------------------------------------------------
    # To use this template, run this command from the top of the ports tree.
    #
    #	git config commit.template .gitmessage
    # -------------------------------------------------------------------------

--- www/apache24/files/README_modules.d
+++ www/apache24/files/README_modules.d
@@ -1,14 +1,15 @@
 # ===================================================
-# Directory for third party module config files.
+# Directory for config files of third party modules.
 #
 # Modules can be disabled by adding a '#' in front
-# of the "LoadModule" line e.g. "#LoadModule"
+# of the "LoadModule" line e.g. "#LoadModule".
 #
 # Files are automatically included if the name
 # begins with a three digit number followed by '_'
-# and ending in '.conf' e.g. '080_mod_php.conf'
+# and ending in '.conf' e.g. '080_mod_php.conf'.
 #
-# Maintainers can also include instructions how to 
-# use the module (instead pkg-message).
+# Maintainers can also include instructions on how to
+# use the module(s) they maintain (instead of doing
+# so in pkg-message).
 #
 
