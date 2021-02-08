44319186d9b5b164fca53948310723a43f713276
Steve Wills <steve@mouf.net>
2021-02-07 19:09:37 -0500

    graphics/lazpaint: update to 7.1.6 release
    PR:		252977
    Submitted by:
    Reported by:
    Reviewed by:
    Approved by:
    Obtained from:
    MFC after:
    MFH:
    Relnotes:
    Security:
    Sponsored by:
    Differential Revision:
    #
    # Description of fields to fill in above:                     76 columns --|
    # PR:                       If and which Problem Report is related.
    # Submitted by:             If someone else sent in the change.
    # Reported by:              If someone else reported the issue.
    # Reviewed by:              If someone else reviewed your modification.
    # Approved by:              If you needed approval for this commit.
    # Obtained from:            If the change is from a third party.
    # MFC after:                N [day[s]|week[s]|month[s]].  Request a reminder email.
    # MFH:                      Ports tree branch name.  Request approval for merge.
    # Relnotes:                 Set to 'yes' for mention in release notes.
    # Security:                 Vulnerability reference (one per line) or description.
    # Sponsored by:             If the change was sponsored by an organization.
    # Differential Revision:    https://reviews.freebsd.org/D### (*full* phabric URL needed).
    #
    # See 'Commit Log Messages' in the Committer's Guide for details.
    #
    # -------------------------------------------------------------------------
    # To use this template, run this command from the top of the ports tree.
    #
    #	git config commit.template .gitmessage
    # -------------------------------------------------------------------------

--- graphics/lazpaint/files/patch-lazpaint_utranslation.pas
+++ graphics/lazpaint/files/patch-lazpaint_utranslation.pas
@@ -1,19 +1,15 @@
---- lazpaint/utranslation.pas	2020-05-13 16:32:15.701490000 -0500
-+++ lazpaint/utranslation.pas	2020-05-13 16:51:08.348905000 -0500
-@@ -60,10 +60,13 @@
-   {$ELSE}
-     {$IFDEF DARWIN}
-     if DirectoryExists(GetDarwinResourcesPath+AResource) then
--      result := GetDarwinResourcesPath+AResource+PathDelim
--    else
-+      result := GetDarwinResourcesPath+AResource+PathDelim;
+--- lazpaint/utranslation.pas.orig	2021-01-24 12:45:19 UTC
++++ lazpaint/utranslation.pas
+@@ -79,10 +79,8 @@ begin
+       result := GetDarwinResourcesPath+AResource+PathDelim
+     else
+     {$ELSE}
+-      {$IFDEF LINUX}
+-      if DirectoryExists(GetLinuxResourcesPath+AResource) then
+-        result := GetLinuxResourcesPath+AResource+PathDelim
+-      else
++      {$IFDEF FREEBSD}
++        result:='%%DATADIR%%'+PathDelim+AResource+PathDelim;
+       {$ENDIF}
      {$ENDIF}
--    result:=ExtractFilePath(Application.ExeName)+AResource+PathDelim;
-+    {$IFDEF FREEBSD}
-+      result:='%%DATADIR%%'+PathDelim+AResource+PathDelim;
-+    {$ELSE}
-+      result:=ExtractFilePath(Application.ExeName)+AResource+PathDelim;
-+    {$ENDIF}
-   {$ENDIF}
- end;
- 
+     result:=ExtractFilePath(Application.ExeName)+AResource+PathDelim;
