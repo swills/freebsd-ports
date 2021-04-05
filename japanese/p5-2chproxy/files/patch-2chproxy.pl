e9e82e963bcf2f522d9317d259f3b2f2a581b753
Steve Wills <steve@mouf.net>
2021-04-04 20:05:12 -0400

    japanese/p5-2chproxy: Fix download failure and update to the latest release
    PR:		254448
    Submitted by:
    Reported by:
    Reviewed by:
    Approved by:
    Obtained from:
    MFC after:
    MFH:
    Relnotes:
    Security:
    Sponsored by:
    Differential Revision:
    #
    # Description of fields to fill in above:                     76 columns --|
    # PR:                       If and which Problem Report is related.
    # Submitted by:             If someone else sent in the change.
    # Reported by:              If someone else reported the issue.
    # Reviewed by:              If someone else reviewed your modification.
    # Approved by:              If you needed approval for this commit.
    # Obtained from:            If the change is from a third party.
    # MFC after:                N [day[s]|week[s]|month[s]].  Request a reminder email.
    # MFH:                      Ports tree branch name.  Request approval for merge.
    # Relnotes:                 Set to 'yes' for mention in release notes.
    # Security:                 Vulnerability reference (one per line) or description.
    # Sponsored by:             If the change was sponsored by an organization.
    # Differential Revision:    https://reviews.freebsd.org/D### (*full* phabric URL needed).
    #
    # See 'Commit Log Messages' in the Committer's Guide for details.
    #
    # -------------------------------------------------------------------------
    # To use this template, run this command from the top of the ports tree.
    #
    #	git config commit.template .gitmessage
    # -------------------------------------------------------------------------

--- japanese/p5-2chproxy/files/patch-2chproxy.pl
+++ japanese/p5-2chproxy/files/patch-2chproxy.pl
@@ -1,8 +1,8 @@
---- 2chproxy.pl.orig	2016-11-16 00:35:06 UTC
+--- 2chproxy.pl.orig	2021-01-19 12:53:34 UTC
 +++ 2chproxy.pl
-@@ -90,8 +90,8 @@ my $PROXY_CONFIG  = {
+@@ -122,8 +122,8 @@ my $PROXY_CONFIG  = {
    HANDLED_COOKIES => [qw(__cfduid yuki PREN)],        #KEEP_COOKIEが有効な時にプロクシで保持するクッキー
-   DAT_URL => '^http://([\w]+)(\.2ch\.net|\.bbspink\.com)(:[\d]+)?/([\w]+)/(?:dat|kako/\d+(?:/\d+)?)/([\d]+(?:-[\d]+)?)\.dat(\.gz)?$',  #datへのアクセスかを判定する正規表現
+   DAT_URL => '^https?://([\w]+)(\.\d+ch\.net|\.bbspink\.com)(:[\d]+)?/([\w]+)/(?:dat|kako/\d+(?:/\d+)?)/([\d]+(?:-[\d]+)?)\.dat(\.gz)?$',  #datへのアクセスかを判定する正規表現
    NULL_DEVICE => '/dev/null',                         #nullデバイスの場所
 -  PID_FILE_NAME => "/tmp/2chproxy.pid",               #pidが書かれたファイル、2重起動禁止にも用いている
 -  LOG_FILE_NAME => "/tmp/2chproxy.log",               #ログファイル
@@ -11,17 +11,3 @@
    NULL_DEVICE_WIN32 => 'nul',                         #nullデバイスの場所(Windows)
    PID_FILE_NAME_WIN32 => dirname($0)."/2chproxy.pid", #pidが書かれたファイル、2重起動禁止にも用いている(Windows)
    LOG_FILE_NAME_WIN32 => dirname($0)."/2chproxy.log", #ログファイル(Windows)
-@@ -325,11 +325,12 @@ sub daemonize() {
-   fork() and exit(0);
-   POSIX::setsid();
-   fork() and exit(0);
--  umask(0);
-+  umask(022);
-   chdir('/');
-   open(STDIN, '<', $null_device_name) or die;
-   open(STDOUT, '>', $log_file_name) or die;
-   open(STDERR, '>', $log_file_name) or die;
-+  chmod(0600, $log_file_name) or die;
- 
-   return 0;
- }
